8655a9cf658ae4fbcbbb8dcc95e516dfca175d97
swathid <swathid@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-07-13 09:50:37 +0000

    DiskSpace.cs,MenuItems.cs
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@148 6b605366-3ce6-ac4f-954a-f758cfb76b34

---
+++ VSNext.Mongo/VSNext.Mongo.Entities/DiskSpace.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Runtime.Serialization;
+using VSNext.Mongo.Repository;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Collections.Generic;
+
+namespace VSNext.Mongo.Entities
+{
+
+    [DataContract]
+    [Serializable]
+    [CollectionName("disk_health")]
+    public class DiskHealth :Entity
+    {
+        [DataMember]
+        [BsonElement("server_name")]
+        public string ServerName { get; set; }
+
+        [DataMember]
+        [BsonElement("server_type")]
+        public string ServerType { get; set; }
+
+        [DataMember]
+        [BsonElement("drives")]
+        public List<Drive> Drives { get; set; }
+
+    }
+    [DataContract]
+    [Serializable]
+    public class Drive
+    {
+        [DataMember]
+        [BsonElement("disk_name")]
+        public string DiskName { get; set; }
+
+        [DataMember]
+        [BsonElement("disk_size")]
+        public double? DiskSize { get; set; }
+
+        [DataMember]
+        [BsonElement("disk_free")]
+        public double? DiskFree
+        {
+            get; set;
+        }
+        [DataMember]
+        [BsonElement("percent_free")]
+        public Double? PercentFree { get; set; }
+
+        [DataMember]
+        [BsonElement("is_marked_for_monitor")]
+        public bool? IsMarkedForMonitor { get; set; }
+
+        [DataMember]
+        [BsonElement("threshold")]
+        public double? Threshold
+        {
+            get; set;
+        }
+        [DataMember]
+        [BsonElement("unit")]
+        public string Unit { get; set; }         
+
+        [DataMember]
+        [BsonElement("last_updated")]
+        public DateTime? LastUpdated { get; set; }       
+    }
+}
