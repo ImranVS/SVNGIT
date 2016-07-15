5052b8a8495606ac1bc7eaae5a375dba38161da8
kiran <kiran@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-07-15 14:20:15 +0000

    Enum is added
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@162 6b605366-3ce6-ac4f-954a-f758cfb76b34

---
+++ VSNext.Mongo/VSNext.Mongo.Entities/Enums.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VSNext.Mongo.Entities
+{
+   public  class Enums
+    {
+
+        public enum ServerType
+        {
+            Domino = 1,
+            BES = 2,
+            Sametime = 3,
+            SharePoint = 4,
+            Exchange = 5,
+            Mail = 6,
+            URL = 7,
+            NetworkDevice = 8,
+            NotesDatabase = 9,
+            General = 10,
+            MobileUsers = 11,
+            DominoClusterDatabase = 12,
+            NotesMailProbe = 13,
+            ExchangeMailFlow = 14,
+            SkypeForBusiness = 15,
+            Windows = 16,
+            Cloud = 17,
+            ActiveDirectory = 18,
+            DatabaseAvailabilityGroup = 19,
+            SNMPDevices = 20,
+            Office365 = 21,
+            WebSphere = 22,
+            NetworkLatency = 23,
+            DominoCluster = 25,
+            ExchangeMailProbe = 26,
+            IBMConnections = 27,
+        }
+
+
+
+
+    }
+}
