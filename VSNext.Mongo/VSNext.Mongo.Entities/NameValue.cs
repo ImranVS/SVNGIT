a2ed84b1fa51272ec4c7d5bde2f1b596c1ed6b9a
nshkarayeva <nshkarayeva@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-08-04 00:27:07 +0000

    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@260 6b605366-3ce6-ac4f-954a-f758cfb76b34

---
+++ VSNext.Mongo/VSNext.Mongo.Entities/NameValue.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VSNext.Mongo.Repository;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Runtime.Serialization;
+
+namespace VSNext.Mongo.Entities
+{
+    [DataContract]
+    [Serializable]
+    [CollectionName("name_value")]
+    public class NameValue : Entity
+    {
+        [DataMember]
+        [BsonElement("name")]
+        [BsonIgnoreIfNullAttribute]
+        public string Name { get; set; }
+
+        [DataMember]
+        [BsonElement("value")]
+        [BsonIgnoreIfNullAttribute]
+        public string Value { get; set; }
+    }
+}
