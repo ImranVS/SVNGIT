d5f1cf99ddfde5e9440486bc824ec189f8e2f84d
kiran <kiran@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-08-30 09:26:48 +0000

    ServerTypeJSON implementation
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@344 6b605366-3ce6-ac4f-954a-f758cfb76b34

---
+++ VitalSignsWeb/VitalSigns.API/Utils/Common.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VitalSigns.API.Utils
+{
+    public class Common
+    {
+        public Dictionary<string, string> GetServerTypeIcons()
+        {
+            Dictionary<string, string> serverTypeIcons = new Dictionary<string, string>();
+
+
+            return serverTypeIcons;
+        }
+    }
+}
