4fa15751dd119e46a33d387ccdf9621484c271e9
kiran <kiran@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-08-30 16:40:57 +0000

    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@349 6b605366-3ce6-ac4f-954a-f758cfb76b34

---
+++ VitalSignsWeb/VitalSigns.API/Models/ServerType.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VitalSigns.API.Models
+{
+    public class ServerType
+    {
+        [JsonProperty("server_type")]
+        public string ServerTypeName { get; set; }
+        [JsonProperty("icon")]
+        public string Icon { get; set; }
+        [JsonProperty("tabs")]
+        public List<Tab> Tabs { get; set; }
+    }
+
+    public class Tab
+    {        
+        [JsonProperty("type")]
+        public string Type { get; set; }
+      
+        [JsonProperty("title")]
+        public string Title { get; set; }
+      
+        [JsonProperty("component")]
+        public string Component { get; set; }
+       
+        [JsonProperty("path")]
+        public string Path { get; set; }
+    }
+}
