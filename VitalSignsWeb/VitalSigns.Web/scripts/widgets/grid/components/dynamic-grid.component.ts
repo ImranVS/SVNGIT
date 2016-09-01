420e5b6b7e8a1997069c5653e66b6ec946ae4991
durga <durga@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-09-01 12:37:06 +0000

    Changes in the Grid component
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@382 6b605366-3ce6-ac4f-954a-f758cfb76b34

--- VitalSignsWeb/VitalSigns.Web/scripts/widgets/grid/components/dynamic-grid.component.ts
+++ VitalSignsWeb/VitalSigns.Web/scripts/widgets/grid/components/dynamic-grid.component.ts
@@ -7,14 +7,14 @@ import {RESTService} from '../../../core/services';
 import * as wjFlexGrid from 'wijmo/wijmo.angular2.grid';
 
 @Component({
-    templateUrl: '/app/widgets/charts/components/dynamic-grid.component.html',
+    templateUrl:'/app/widgets/grid/components/dynamic-grid.component.html' ,
     directives: [wjFlexGrid.WjFlexGrid, wjFlexGrid.WjFlexGridColumn],
     providers: [
         HTTP_PROVIDERS,
         RESTService
     ]
 })
-export class SampleGrid implements WidgetComponent, OnInit {
+export class DynamicGrid implements WidgetComponent, OnInit {
     @Input() settings: any;
 
     data: wijmo.collections.CollectionView;
@@ -23,10 +23,10 @@ export class SampleGrid implements WidgetComponent, OnInit {
     constructor(private service: RESTService) { }
 
     ngOnInit() {
-
+       // alert(this.settings.columns[0].header);
         this.service.get(this.settings.url)
             .subscribe(
-            response => this.data = new wijmo.collections.CollectionView(new wijmo.collections.ObservableArray(response.data)),
+            data => this.data = new wijmo.collections.CollectionView(new wijmo.collections.ObservableArray(data)),
             error => this.errorMessage = <any>error
             );
 
