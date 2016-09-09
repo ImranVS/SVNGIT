07d9dec2a0fe1cd29e4100afef73ee9d45e58c00
sowjanya <sowjanya@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-09-09 14:33:33 +0000

    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@456 6b605366-3ce6-ac4f-954a-f758cfb76b34

---
+++ VitalSignsWeb/VitalSigns.API/Models/MonitoredTasks.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace VitalSigns.API.Models
+{
+    public class MonitoredTasks
+    {
+
+        //[JsonProperty("_id")]
+        //public int? ID { get; set; }
+
+        [JsonProperty("task_name")]
+        public string TaskName { get; set; }
+
+        [JsonProperty("monitored")]
+        public Boolean? Monitored { get; set; }
+
+        [JsonProperty("primary_status")]
+        public string PrimaryStatus { get; set; }
+
+        [JsonProperty("secondary_status")]
+        public string SecondaryStatus { get; set; }
+
+
+        [JsonProperty("last_updated")]
+        public string LastUpdated { get; set; }
+
+       
+        
+
+     
+    }
+}
+
