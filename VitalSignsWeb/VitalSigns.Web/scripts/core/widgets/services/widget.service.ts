4fda4cf4a559e0d3ad83400b423d9b72b06a8573
ometral <ometral@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-09-12 23:18:23 +0000

    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@484 6b605366-3ce6-ac4f-954a-f758cfb76b34

---
+++ VitalSignsWeb/VitalSigns.Web/scripts/core/widgets/services/widget.service.ts
@@ -0,0 +1,75 @@
+﻿import { Injectable } from '@angular/core';
+
+import {WidgetComponent} from '../models/widget-component'
+import {WidgetContract} from '../models/widget-contract';
+import {WidgetController} from '../components/widget-controller';
+
+export interface WidgetServiceItem {
+
+    controller: WidgetController;
+    contract: WidgetContract;
+    component: WidgetComponent;
+
+}
+
+@Injectable()
+export class WidgetService {
+
+    private _widgets: WidgetServiceItem[] = [];
+    
+    exists(widgetId: string): boolean {
+
+        return this._widgets.some(widget => widget.contract.id == widgetId);
+
+    }
+
+    refreshWidget(widgetId: string, settings?: any) {
+
+        let widgetRef = this.findWidget(widgetId);
+
+        return new Promise(function (resolve, reject) {
+        
+            if (!widgetRef)
+                reject(`Widget ${widgetId} does not exist or is not registered.`);
+
+            if (widgetRef.component.refresh) {
+                widgetRef.component.refresh(settings);
+                resolve();
+            }
+
+            reject(`Widget ${widgetId} does not support refresh.`);
+            
+        });
+        
+    }
+
+    findWidget(widgetId: string): WidgetServiceItem {
+    
+        let widget = this._widgets
+            .find(widget => widget.contract.id == widgetId);
+
+        if (widget)
+            return widget;
+
+    }
+    
+    registerWidget(controller: WidgetController, contract: WidgetContract, component: WidgetComponent) {
+
+        if (this.exists(contract.id))
+            throw new Error(`Widget identifier (${contract.id}) already in use.`);
+
+        this._widgets.push({
+            controller: controller,
+            contract: contract,
+            component: component
+        });
+
+    }
+
+    unloadController(controller: WidgetController) {
+    
+        this._widgets = this._widgets.filter(widget => widget.controller !== controller);
+            
+    }
+    
+}
