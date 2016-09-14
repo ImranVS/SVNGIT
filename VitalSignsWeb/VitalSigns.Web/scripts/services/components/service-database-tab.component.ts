3a5451c3615d1f5db4167f9ba9802af1f15c8177
kiran <kiran@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-09-14 10:01:54 +0000

    Grid Implementation
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@494 6b605366-3ce6-ac4f-954a-f758cfb76b34

--- VitalSignsWeb/VitalSigns.Web/scripts/services/components/service-database-tab.component.ts
+++ VitalSignsWeb/VitalSigns.Web/scripts/services/components/service-database-tab.component.ts
@@ -31,32 +31,11 @@ export class ServiceDatabaseTab extends WidgetController implements OnInit {
         });
         this.widgets = [
             {
-                id: 'dynamicGrid',
+                id: 'ServiceDatabaseGrid',
                 title: 'All Databases',
-                path: '/app/widgets/grid/components/dynamic-grid.component',
-                name: 'DynamicGrid',
-                css: 'col-xs-12',
-                settings: {
-                    url: '/DashBoard/' + this.deviceId +'/database',
-                    //url: '/DashBoard/57af28415c6c6c02d4fce747/database',
-                    columns: [
-                        { header: "Title", binding: "title", name: "title", width: "*" },
-                        { header: "Status", binding: "status", name: "status", width: "*" },
-                        { header: "DeviceName", binding: "device_name", name: "device_name", width: "*" },
-                        { header: "Folder", binding: "folder", name: "folder", width: "*" },
-                        { header: "FolderCount", binding: "folder_count", name: "folder_count", width: "*" },
-                        { header: "Details", binding: "details", name: "details", width: "*" },
-                        { header: "FileName", binding: "file_name", name: "file_name", width: "*" },
-                        { header: "DesignTemplateName", binding: "design_template_name", name: "design_template_name", width: "*" },
-                        { header: "FileSize", binding: "file_size", name: "file_size", width: "*" },
-                        { header: "Quota", binding: "quota", name: "quota", width: "*" },
-                        { header: "InboxDocCount", binding: "inbox_doc_count", name: "inbox_doc_count", width: "*" },
-                        { header: "ScanDateTime", binding: "scan_date", name: "scan_date", width: "*" },
-                        { header: "ReplicaId", binding: "replica_idss", name: "replica_idss", width: "*" },
-                        { header: "DocumentCount", binding: "document_count", name: "document_count", width: "*" },
-                        { header: "Categories", binding: "categories", name: "categories", width: "*" }
-                    ]
-                }
+                path: '/app/services/components/service-database-grid.component',
+                name: 'ServiceDatabaseGrid',
+                css: 'col-xs-12'               
             }
         ]
         injectSVG();
