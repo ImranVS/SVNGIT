89768706cb3e354d527842d753f3c8cece0b9d30
swathid <swathid@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-09-19 15:06:28 +0000

    Server Roles
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@532 6b605366-3ce6-ac4f-954a-f758cfb76b34

--- VitalSignsWeb/VitalSigns.Web/scripts/dashboards/components/ibm-domino/ibm-domino-dashboard.component.ts
+++ VitalSignsWeb/VitalSigns.Web/scripts/dashboards/components/ibm-domino/ibm-domino-dashboard.component.ts
@@ -34,7 +34,7 @@ export class IBMDominoDashboard extends WidgetController implements OnInit {
             name: 'ChartComponent',
             css: 'col-xs-12 col-sm-6 col-md-6 col-lg-4',
             settings: {
-                url: 'http://private-ad10c-ibm.apiary-mock.com/domino/server_roles',
+                url: '/services/status_count?type=Domino&docfield=secondary_role',
                 chart: {
                     chart: {
                         renderTo: 'serverRoles',
@@ -86,7 +86,7 @@ export class IBMDominoDashboard extends WidgetController implements OnInit {
             name: 'ChartComponent',
             css: 'col-xs-12 col-sm-6 col-md-6 col-lg-4',
             settings: {
-                url: 'http://private-ad10c-ibm.apiary-mock.com/domino/status',
+                url: '/services/status_count?type=Domino&docfield=status_code',
                 chart: {
                     chart: {
                         renderTo: 'serverStatus',
@@ -138,7 +138,7 @@ export class IBMDominoDashboard extends WidgetController implements OnInit {
             name: 'ChartComponent',
             css: 'col-xs-12 col-sm-6 col-md-6 col-lg-4',
             settings: {
-                url: 'http://private-ad10c-ibm.apiary-mock.com/domino/os',
+                url: '/services/status_count?type=Domino&docfield=operating_system',
                 chart: {
                     chart: {
                         renderTo: 'serverOs',
