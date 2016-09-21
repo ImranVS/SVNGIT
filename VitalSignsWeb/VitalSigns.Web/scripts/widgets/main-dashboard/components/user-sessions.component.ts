98dbe7f77ac49f00209b0431419a2176c18da669
sowjanya <sowjanya@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-09-21 10:42:56 +0000

    Added  Usersessions, Downtime, Average Response Widgets in Dashboard
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@556 6b605366-3ce6-ac4f-954a-f758cfb76b34

---
+++ VitalSignsWeb/VitalSigns.Web/scripts/widgets/main-dashboard/components/user-sessions.component.ts
@@ -0,0 +1,49 @@
+﻿import {Component, Input, OnInit, AfterViewChecked} from '@angular/core';
+import {HTTP_PROVIDERS}    from '@angular/http';
+
+import {WidgetComponent} from '../../../core/widgets';
+import {RESTService} from '../../../core/services';
+
+declare var injectSVG: any;
+
+@Component({
+    templateUrl: './app/widgets/main-dashboard/components/user-sessions.component.html',
+    providers: [
+        HTTP_PROVIDERS,
+        RESTService
+    ]
+})
+export class UserSessions implements WidgetComponent, OnInit, AfterViewChecked {
+    @Input() settings: any;
+
+    errorMessage: string;
+    responses: string;
+
+    usersessionsStatus: any;
+
+    constructor(private service: RESTService) { }
+
+    loadData() {
+
+        this.service.get('/services/dashboard_stats')
+            .subscribe(
+            response => this.usersessionsStatus = response.data,
+          
+            error => this.errorMessage = <any>error
+        );
+      
+    }
+
+
+    ngOnInit() {
+        this.loadData();
+        
+      
+    }
+
+    ngAfterViewChecked() {
+        injectSVG();
+    }
+
+    
+}
