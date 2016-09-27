40dbbfc1148f9ceb66ec8861687131027f0f0f8f
ometral <ometral@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-09-27 20:02:11 +0000

    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@603 6b605366-3ce6-ac4f-954a-f758cfb76b34

---
+++ VitalSignsWeb/VitalSigns.Web/scripts/services/service-tab.collection.ts
@@ -0,0 +1,19 @@
+﻿export * from './components/domino-health-tab.component';
+export * from './components/no-selected-service.component';
+export * from './components/service-overall-tab.component';
+
+export * from '../dashboards/components/ibm-connections/ibm-connections-overview-tab.component';
+export * from '../dashboards/components/ibm-connections/ibm-connections-communities-tab.component';
+
+export * from '../dashboards/components/ibm-sametime/ibm-sametime-chats-tab.component';
+export * from '../dashboards/components/ibm-sametime/ibm-sametime-conferences-tab.component';
+export * from '../dashboards/components/ibm-sametime/ibm-sametime-meetings-tab.component';
+export * from '../dashboards/components/ibm-sametime/ibm-sametime-overall-tab.component';
+
+export * from '../not-yet-implemented.component';
+
+export * from './components/service-mainhealth-tab.component';
+export * from './components/service-database-tab.component';
+export * from './components/service-servertasks-tab.component';
+export * from './components/service-outages-tab.component';
+
