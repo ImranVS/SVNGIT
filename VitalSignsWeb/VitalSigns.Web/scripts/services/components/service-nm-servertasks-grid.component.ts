0aebdee0be99f0a3143f7f2dd7181d5eec639c0d
nshkarayeva <nshkarayeva@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-09-27 17:50:08 +0000

    Added Non Monitored Tasks component.
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@599 6b605366-3ce6-ac4f-954a-f758cfb76b34

---
+++ VitalSignsWeb/VitalSigns.Web/scripts/services/components/service-nm-servertasks-grid.component.ts
@@ -0,0 +1,89 @@
+﻿import {Component, Input, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {HTTP_PROVIDERS}    from '@angular/http';
+import {WidgetComponent, WidgetService} from '../../core/widgets';
+import {RESTService} from '../../core/services';
+import {AppNavigator} from '../../navigation/app.navigator.component';
+import {ServiceTab} from '../models/service-tab.interface';
+import * as wjFlexGrid from 'wijmo/wijmo.angular2.grid';
+import * as wjFlexGridFilter from 'wijmo/wijmo.angular2.grid.filter';
+import * as wjFlexGridGroup from 'wijmo/wijmo.angular2.grid.grouppanel';
+import * as wjFlexInput from 'wijmo/wijmo.angular2.input';
+
+declare var injectSVG: any;
+declare var bootstrapNavigator: any;
+
+@Component({
+    templateUrl: '/app/services/components/service-nm-servertasks-grid.component.html',
+    directives: [
+        wjFlexGrid.WjFlexGrid,
+        wjFlexGrid.WjFlexGridColumn,
+        wjFlexGrid.WjFlexGridCellTemplate,
+        wjFlexGridFilter.WjFlexGridFilter,
+        wjFlexGridGroup.WjGroupPanel,
+        wjFlexInput.WjMenu,
+        wjFlexInput.WjMenuItem,
+        AppNavigator
+    ],
+    providers: [
+        HTTP_PROVIDERS,
+        RESTService
+    ]
+})
+export class ServiceNMServerTasksGrid implements OnInit {
+    @Input() settings: any;
+    deviceId: any;
+    //data: wijmo.collections.CollectionView;
+    data: wijmo.collections.CollectionView;
+    errorMessage: string;
+
+    constructor(private service: RESTService, private widgetService: WidgetService, private route: ActivatedRoute) { }
+
+    get pageSize(): number {
+
+        return this.data.pageSize;
+
+    }
+
+
+
+    set pageSize(value: number) {
+        if (this.data.pageSize != value) {
+            this.data.pageSize = value;
+            this.data.refresh();
+        }
+    }
+
+    ngOnInit() {
+
+       
+        this.route.params.subscribe(params => {
+            this.deviceId = params['service'];
+
+        });
+        this.service.get('/DashBoard/' + this.deviceId + '/monitoredtasks?is_monitored=false')
+            .subscribe(
+            (response) => {
+                //this.data = new wijmo.collections.CollectionView(new wijmo.collections.ObservableArray(response.data));
+                this.data = new wijmo.collections.CollectionView(new wijmo.collections.ObservableArray(response.data));
+                this.data.pageSize = 10;
+            },
+            (error) => this.errorMessage = <any>error
+            );
+
+    }
+
+    getAccessColor(access: string) {
+
+        switch (access) {
+            case 'Allow':
+                return 'green';
+            case 'Blocked':
+                return 'red';
+            default:
+                return '';
+        }
+
+    }
+
+}
