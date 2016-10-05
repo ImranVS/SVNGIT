a97b298a8d53d92ba9017c280c3e490d78a245e3
durga <durga@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-10-05 09:30:13 +0000

    IBM Domino Settings
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@631 6b605366-3ce6-ac4f-954a-f758cfb76b34

---
+++ VitalSignsWeb/VitalSigns.API/Models/Configurator/NameValue.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace VitalSigns.API.Models
+{
+    public class NameValueModel
+    {
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("value")]
+        public string Value { get; set; }
+
+        [JsonProperty("category")]
+        public string Category { get; set; }
+    }
+}
