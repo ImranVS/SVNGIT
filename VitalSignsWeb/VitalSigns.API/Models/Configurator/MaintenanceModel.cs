057a3ad58bec05067ce46adaf2e26adc7b40f81b
sowjanya <sowjanya@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-10-05 11:59:00 +0000

    Maintenance model added
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@636 6b605366-3ce6-ac4f-954a-f758cfb76b34

---
+++ VitalSignsWeb/VitalSigns.API/Models/Configurator/MaintenanceModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace VitalSigns.API.Models
+{
+    public class MaintenanceModel
+    {
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("start_date")]
+        public DateTime StartDate { get; set; }
+
+        [JsonProperty("start_time")]
+        public DateTime StartTime { get; set; }
+
+        [JsonProperty("end_date")]
+        public DateTime EndDate { get; set; }
+
+        [JsonProperty("duration")]
+        public int Duration { get; set; }
+
+        [JsonProperty("maintenance_frequency")]
+        public int MaintenanceFrequency { get; set; }
+
+        [JsonProperty("maintenance_days_list")]
+        public string MaintenanceDaysList { get; set; }
+
+        [JsonProperty("continue_forever")]
+        public bool ContinueForever { get; set; }
+
+    }
+}
