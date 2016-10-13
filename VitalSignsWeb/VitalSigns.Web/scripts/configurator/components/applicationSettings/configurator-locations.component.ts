eee7b253400e164f64d920dd76644f02a00394ab
swathid <swathid@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-10-13 10:13:49 +0000

    Added Locations
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@676 6b605366-3ce6-ac4f-954a-f758cfb76b34

---
+++ VitalSignsWeb/VitalSigns.Web/scripts/configurator/components/applicationSettings/configurator-locations.component.ts
@@ -0,0 +1,98 @@
+﻿import {Component, OnInit, ViewChild, AfterViewInit} from '@angular/core';
+import {FormBuilder, FormGroup, FormControl, REACTIVE_FORM_DIRECTIVES, REACTIVE_FORM_PROVIDERS, Validators} from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import {HTTP_PROVIDERS}    from '@angular/http';
+import {RESTService} from '../../core/services';
+import {AppNavigator} from '../../navigation/app.navigator.component';
+import * as wjFlexGrid from 'wijmo/wijmo.angular2.grid';
+import * as wjFlexGridFilter from 'wijmo/wijmo.angular2.grid.filter';
+import * as wjFlexGridGroup from 'wijmo/wijmo.angular2.grid.grouppanel';
+import * as wjFlexInput from 'wijmo/wijmo.angular2.input';
+import * as wjCoreModule from 'wijmo/wijmo.angular2.core';
+import {GridBase} from '../../core/gridBase';
+
+
+
+@Component({
+    templateUrl: '/app/configurator/components/configurator-locations.component.html',
+    directives: [REACTIVE_FORM_DIRECTIVES,
+        wjFlexGrid.WjFlexGrid,
+        wjFlexGrid.WjFlexGridColumn,
+        wjFlexGrid.WjFlexGridCellTemplate,
+        wjFlexGridFilter.WjFlexGridFilter,
+        wjFlexGridGroup.WjGroupPanel,
+        wjFlexInput.WjMenu,
+        wjFlexInput.WjMenuItem,
+		wjFlexInput.WjComboBox,
+        AppNavigator
+    ],
+    providers: [
+        HTTP_PROVIDERS,
+        RESTService
+    ]
+})
+export class Location extends GridBase implements OnInit  {
+    @ViewChild('flex') flex: wijmo.grid.FlexGrid;
+    data: wijmo.collections.CollectionView;
+    selectedCountry: string;
+    selectedState: string;
+    errorMessage: string;
+    locationsForm: FormGroup;
+    locationsId: string;
+    countries: any;
+    states: any;
+    get pageSize(): number {
+        return this.data.pageSize;
+    }
+
+    constructor(service: RESTService) {
+        super(service, '/Configurator/locations');
+        this.formName = "Maintain Locations";
+
+        this.service.get('/Configurator/get_locations')
+            .subscribe(
+            (response) => {
+                this.countries = response.data.countryData;
+                //delete this.deviceTypes[0];
+                alert(this.countries);
+                this.countries.splice(0, 1);
+
+
+
+            },
+            (error) => this.errorMessage = <any>error
+        );
+        this.service.get('/Configurator/get_locations?' + this.selectedCountry + '')
+            .subscribe(
+            (response) => {
+                this.states = response.data.stateData;
+                //delete this.deviceTypes[0];
+                alert(this.states);
+                this.states.splice(0, 1);
+
+
+            },
+            (error) => this.errorMessage = <any>error
+            );   
+
+    }
+
+
+    savelocations() {
+        this.saveGridRow('/Configurator/save_locations');
+    }
+
+    ngOnInit() {
+
+
+    }
+
+
+
+    deltelocations() {
+
+        this.delteGridRow('/Configurator/delete_location/');
+
+    }
+}
