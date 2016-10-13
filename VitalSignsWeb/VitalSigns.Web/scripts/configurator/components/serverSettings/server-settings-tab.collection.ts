8f15135b4caa7f634adc511216072a4df43a168a
kiran <kiran@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-10-13 12:02:55 +0000

    Server Setings tabs
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@693 6b605366-3ce6-ac4f-954a-f758cfb76b34

---
+++ VitalSignsWeb/VitalSigns.Web/scripts/configurator/components/serverSettings/server-settings-tab.collection.ts
@@ -0,0 +1,9 @@
+﻿
+export * from '../../../not-yet-implemented.component';
+
+
+
+
+
+
+
---
+++ VitalSignsWeb/VitalSigns.Web/scripts/configurator/components/serverSettings/server-settings-tabs.component.ts
@@ -0,0 +1,75 @@
+﻿import {Component, OnInit, ComponentFactoryResolver, ComponentFactory, ElementRef, ComponentRef, ViewChild, ViewContainerRef} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {HttpModule}    from '@angular/http';
+
+import {RESTService} from '../../../core/services';
+
+import * as ServiceTabs from './server-settings-tab.collection';
+
+@Component({
+    templateUrl: '/app/configurator/components/serverSettings/server-settings-tabs.component.html',
+    providers: [
+        HttpModule,
+        RESTService
+    ]
+})
+export class ServerSettings implements OnInit {
+    @ViewChild('tab', { read: ViewContainerRef }) target: ViewContainerRef;
+
+    tabsData: any;
+    activeTabComponent: ComponentRef<{}>;
+    constructor(private resolver: ComponentFactoryResolver, private elementRef: ElementRef) { }
+    selectTab(tab: any) {
+        // Activate selected tab
+        this.tabsData.forEach(tab => tab.active = "false");
+        tab.active = "true";
+        // Dispose current tab if one already active
+        if (this.activeTabComponent)
+            this.activeTabComponent.destroy();
+
+        // Lazy-load selected tab component
+       // console.log(tab.component);
+        console.log(ServiceTabs[tab.component]);
+        let factory = this.resolver.resolveComponentFactory(ServiceTabs[tab.component]);
+        console.log(factory);
+        this.activeTabComponent = this.target.createComponent(factory);
+    }
+    ngOnInit() {   
+        this.tabsData= [
+            {
+                "title": "Server Atributes",
+                "component": "NotYetImplemented",
+                "path": "/app/not-yet-implemented.component",
+                "active": "true"
+            },
+            {
+                "title": "Domino Server Tasks",
+                "component": "NotYetImplemented",
+                "path": "/app/not-yet-implemented.component",
+                "active": "false"
+            },
+            {
+                "title": "Windows Services",
+                "component": "NotYetImplemented",
+                "path": "/app/not-yet-implemented.component",
+                "active": "false"
+            },
+            {
+                "title": "Disk Settings",
+                "component": "NotYetImplemented",
+                "path": "/app/not-yet-implemented.component",
+                "active": "false"
+            },
+            {
+                "title": "Location/Credentials/Business Hours",
+                "component": "NotYetImplemented",
+                "path": "/app/not-yet-implemented.component",
+                "active": "false"
+            }
+
+        ];
+        console.log(this.tabsData);
+              this.selectTab(this.tabsData[0]);
+             
+        };
+}
