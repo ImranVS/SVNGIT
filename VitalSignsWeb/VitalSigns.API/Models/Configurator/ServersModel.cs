da9264d6dc509cb6034104c5742a322f8a2dd71d
sowjanya <sowjanya@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-10-13 14:07:23 +0000

    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@700 6b605366-3ce6-ac4f-954a-f758cfb76b34

---
+++ VitalSignsWeb/VitalSigns.API/Models/Configurator/ServersModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace VitalSigns.API.Models
+{
+    public class ServersModel
+    {
+
+
+        [JsonProperty("device_name")]
+        public string ServerName { get; set; }
+
+        [JsonProperty("device_type")]
+        public string ServerType { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+
+        [JsonProperty("server_id")]
+        public List<string> ServerId { get; set; }
+
+
+    }
+}
