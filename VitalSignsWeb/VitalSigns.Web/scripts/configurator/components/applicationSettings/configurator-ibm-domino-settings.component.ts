140be4bea6e90451056c8394b770e11c1212a1e3
durga <durga@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-10-13 14:49:23 +0000

    IBM Domino settings
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@704 6b605366-3ce6-ac4f-954a-f758cfb76b34

--- VitalSignsWeb/VitalSigns.Web/scripts/configurator/components/applicationSettings/configurator-ibm-domino-settings.component.ts
+++ VitalSignsWeb/VitalSigns.Web/scripts/configurator/components/applicationSettings/configurator-ibm-domino-settings.component.ts
@@ -30,43 +30,57 @@ export class IbmDominoSettingsForm implements OnInit, AfterViewInit {
         private route: ActivatedRoute) {
 
         this.ibmDominoSettingsForm = this.formBuilder.group({
-            'name': ['', Validators.required],
-            'value': ['', Validators.required],
+            'notes_program_directory': [''],
+            'notes_user_id': [''],
+            'notes_ini': [''],
+            'notes_password': [''],
+            'enableex_journal': [''],
+            'enable_domino_console_commands': [''],
+            'exjournal_threshold': [''],
+            'consecutive_telnet': ['']
+            //'id': [''],
+            //'name': [''],
+            //'value': [''],
+            //'category': ['']
+          
+
         });
 
     }
 
     ngOnInit() {
-
+      
+       // alert("Hi");
         this.route.params.subscribe(params => {
 
             this.profileEmail = params['email'];
-
+            
            
-                this.formTitle = "IBM Domino Settings";
+            this.formTitle = "IBM Domino Settings";
+          //  this.dataProvider.get('/services/get_ibm_domino_settings')
                 this.dataProvider.get('/services/get_ibm_domino_settings')
                     .subscribe(
-                    (data) => this.ibmDominoSettingsForm.setValue(data),
+                    (data) => this.ibmDominoSettingsForm.setValue(data.data),
                     (error) => this.errorMessage = <any>error
+                   
                     );
 
-               
+              
         });
 
     }
 
     ngAfterViewInit() {
 
-        if (this.insertMode)
-            this.inputName.first.nativeElement.focus();
+    
 
     }
 
-    onSubmit(profile: any): void {
+    onSubmit(nameValue: any): void {
 
         this.dataProvider.put(
-            `http://localhost:1234/profiles/${profile.email}`,
-            profile,
+            '/services/get_ibm_domino_settings',
+            nameValue,
             () => this.router.navigate(['profiles']));
 
     }
