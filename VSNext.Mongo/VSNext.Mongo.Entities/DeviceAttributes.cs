056fad63fea1a29ae4dd94594f6a93036c8e5b95
sowjanya <sowjanya@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-10-14 13:04:18 +0000

    Added ServerAttributes Entity
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@730 6b605366-3ce6-ac4f-954a-f758cfb76b34

---
+++ VSNext.Mongo/VSNext.Mongo.Entities/DeviceAttributes.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Runtime.Serialization;
+using VSNext.Mongo.Repository;
+
+namespace VSNext.Mongo.Entities
+{
+    [DataContract]
+    [Serializable]
+    [CollectionName("device_attributes")]
+    public class DeviceAttributes : Entity
+    {
+        [DataMember]
+        [BsonElement("attribute_name")]
+        public string AttributeName { get; set; }
+
+        [DataMember]
+        [BsonElement("device_type")]
+        public string DeviceType { get; set; }
+
+        [DataMember]
+        [BsonElement("unitofmeasurement")]
+        public int Unitofmeasurement { get; set; }
+
+        [DataMember]
+        [BsonElement("default_value")]
+        public int DefaultValue { get; set; }
+    }
+}
