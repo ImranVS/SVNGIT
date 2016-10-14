bf35d5d3f40a027001ce79c9d97e4d9ee1cf6c17
sowmya <sowmya@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-10-14 09:18:25 +0000

    Maintain Pages
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@717 6b605366-3ce6-ac4f-954a-f758cfb76b34

---
+++ VSNext.Mongo/VSNext.Mongo.Entities/Users.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Runtime.Serialization;
+using VSNext.Mongo.Repository;
+
+namespace VSNext.Mongo.Entities
+{
+    [DataContract]
+    [Serializable]
+    [CollectionName("users")]
+    public class Users : Entity
+    {
+        [DataMember]
+        [BsonElement("login_name")]
+        [BsonIgnoreIfNull]
+        public string LoginName { get; set; }
+
+        [DataMember]
+        [BsonElement("full_name")]
+        [BsonIgnoreIfNull]
+        public string FullName { get; set; }
+
+        [DataMember]
+        [BsonElement("email")]
+        [BsonIgnoreIfNull]
+        public string Email { get; set; }
+
+        [DataMember]
+        [BsonElement("status")]
+        [BsonIgnoreIfNull]
+        public string Status { get; set; }
+
+        [DataMember]
+        [BsonElement("super_admin")]
+        [BsonIgnoreIfNull]
+        public string SuperAdmin { get; set; }
+
+        [DataMember]
+        [BsonElement("configurator_access")]
+        [BsonIgnoreIfNull]
+        public bool ConfiguratorAccess { get; set; }
+
+        [DataMember]
+        [BsonElement("console_command_access")]
+        [BsonIgnoreIfNull]
+        public bool ConsoleCommandAccess { get; set; }
+
+    }
+}
