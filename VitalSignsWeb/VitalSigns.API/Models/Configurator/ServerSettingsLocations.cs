01cdf1c6d380ce68cdc4d3c6b4f04fa1cc96d707
durga <durga@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-10-15 10:35:22 +0000

    SSE Locations/Credentials/Business Hours
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@751 6b605366-3ce6-ac4f-954a-f758cfb76b34

---
+++ VitalSignsWeb/VitalSigns.API/Models/Configurator/ServerSettingsLocations.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace VitalSigns.API.Models
+{
+    public class ServerSettingsLocations
+    {
+
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("type_id")]
+        public string TypeId { get; set; }
+
+        [JsonProperty("servers_list")]
+        public string ServersList { get; set; }
+
+       
+    }
+}
