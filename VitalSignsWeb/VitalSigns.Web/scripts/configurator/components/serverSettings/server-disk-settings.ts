5a793287685de75fca6547f6017071a272faca74
durga <durga@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-10-19 09:17:22 +0000

    SSE Disk Settings
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@815 6b605366-3ce6-ac4f-954a-f758cfb76b34

--- VitalSignsWeb/VitalSigns.Web/scripts/configurator/components/serverSettings/server-disk-settings.ts
+++ VitalSignsWeb/VitalSigns.Web/scripts/configurator/components/serverSettings/server-disk-settings.ts
@@ -1,9 +1,10 @@
-﻿import {Component, OnInit, AfterViewInit, ViewChildren,Output, EventEmitter} from '@angular/core';
+﻿import {Component, OnInit, AfterViewInit, ViewChild,Output, EventEmitter} from '@angular/core';
 import {FormBuilder, FormGroup, FormControl, Validators} from '@angular/forms';
 import {Router, ActivatedRoute} from '@angular/router';
 import {HttpModule}    from '@angular/http';
 import {GridBase} from '../../../core/gridBase';
 import {RESTService} from '../../../core/services';
+import {DiskSttingsValue} from '../../models/server-disk-settings';
 
 @Component({
     selector: 'servder-form',
@@ -14,9 +15,9 @@ import {RESTService} from '../../../core/services';
     ]
 })
 //export class ServerDiskSettings implements OnInit, AfterViewInit {
-export class ServerDiskSettings extends GridBase implements OnInit  {
+export class ServerDiskSettings  {
     
-    @ViewChildren('name') inputName; 
+    data: wijmo.collections.CollectionView;
     errorMessage: any;
     deviceLocationData: any;
     deviceCredentialData: any;
@@ -30,15 +31,21 @@ export class ServerDiskSettings extends GridBase implements OnInit  {
     selectedDisks: string;
     noDiskAlerts: string;
     postData: any;
-    
+    diskValues: any;
+    @ViewChild('flex') flex: wijmo.grid.FlexGrid;
+
     constructor(    
         private dataProvider: RESTService,
         private formBuilder: FormBuilder) {
 
-        super(dataProvider, '/Configurator/get_disk_names');
+        this.dataProvider.get('/Configurator/get_disk_names')
+            .subscribe(
+            response => {
+                this.data = new wijmo.collections.CollectionView(new wijmo.collections.ObservableArray(response.data));
+                this.data.pageSize = 10;
+
+            }); 
       
-       // super(dataProvider, '/Configurator/get_disk_names');
-       this.formName = "Disk Information";
         this.diskSettingsForm = this.formBuilder.group({
             'setting': [''],
             'value': [''],
@@ -48,13 +55,11 @@ export class ServerDiskSettings extends GridBase implements OnInit  {
         });
    }
 
-    ngOnInit() {
-      
-    }
-
+   
     
     applySetting(nameValue: any): void{
-
+       // alert(this.flex);
+       
         if (this.selectedDiskSetting == "allDisksBypercentage")
         {
             alert(this.diskByPercentage);
@@ -63,14 +68,45 @@ export class ServerDiskSettings extends GridBase implements OnInit  {
         else if (this.selectedDiskSetting == "allDisksByGB")
             this.selectedDiskSettingValue = this.diskByGB;
         else if (this.selectedDiskSetting == "selectedDisks")
-            this.selectedDiskSettingValue = this.selectedDisks;  
+        {
+            this.selectedDiskSettingValue = this.selectedDisks;
+            var slectedDiskSettingValues: DiskSttingsValue[] = [];
+            for (var _i = 0; _i < this.flex.collectionView.sourceCollection.length; _i++) {
+
+                var item = (<wijmo.collections.CollectionView>this.flex.collectionView.sourceCollection)[_i];
+
+                if (item.is_selected) {
+
+                    var dominoserverObject = new DiskSttingsValue();
+                    dominoserverObject.is_selected = item.is_selected;
+                    dominoserverObject.disk_name = item.disk_name;
+                    dominoserverObject.freespace_threshold = item.freespace_threshold;
+                    dominoserverObject.threshold_type = item.threshold_type;
+
+                    slectedDiskSettingValues.push(dominoserverObject);
+                }
+
+            }
+            this.diskValues = slectedDiskSettingValues;
+        }
         else if (this.selectedDiskSetting == "noDiskAlerts")
             this.selectedDiskSettingValue = this.noDiskAlerts;  
-        this.postData = {
-            "setting": this.selectedDiskSetting,
-            "value": this.selectedDiskSettingValue,
-            "devices": this.devices
-        }; 
+        if (this.selectedDiskSetting == "selectedDisks")
+        {
+
+            this.postData = {
+                "setting": this.selectedDiskSetting,
+                "value": this.diskValues,
+                "devices": this.devices
+            };
+        }
+        else {
+            this.postData = {
+                "setting": this.selectedDiskSetting,
+                "value": this.selectedDiskSettingValue,
+                "devices": this.devices
+            };
+        }
       
         this.diskSettingsForm.setValue(this.postData);
         this.dataProvider.put(
@@ -80,5 +116,15 @@ export class ServerDiskSettings extends GridBase implements OnInit  {
     changeInDevices(server: string) {
         this.devices = server;
     }
-   
+    get pageSize(): number {
+        return this.data.pageSize;
+    }
+    set pageSize(value: number) {
+        if (this.data.pageSize != value) {
+            this.data.pageSize = value;
+            if (this.flex) {
+                (<wijmo.collections.IPagedCollectionView>this.flex.collectionView).pageSize = value;
+            }
+        }
+    }
 }
