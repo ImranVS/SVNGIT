c6e04c01c1a1fd2e6a23cb709d9256046f345557
nshkarayeva <nshkarayeva@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-10-19 04:36:03 +0000

    Events tab.
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@812 6b605366-3ce6-ac4f-954a-f758cfb76b34

---
+++ VitalSignsWeb/VitalSigns.Web/scripts/services/components/service-events-grid.component.ts
@@ -0,0 +1,70 @@
+﻿import {Component, Input, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {HttpModule}    from '@angular/http';
+import {WidgetComponent, WidgetService} from '../../core/widgets';
+import {RESTService} from '../../core/services';
+import {AppNavigator} from '../../navigation/app.navigator.component';
+import {ServiceTab} from '../models/service-tab.interface';
+import * as wjFlexGrid from 'wijmo/wijmo.angular2.grid';
+import * as wjFlexGridFilter from 'wijmo/wijmo.angular2.grid.filter';
+import * as wjFlexGridGroup from 'wijmo/wijmo.angular2.grid.grouppanel';
+import * as wjFlexInput from 'wijmo/wijmo.angular2.input';
+
+declare var injectSVG: any;
+declare var bootstrapNavigator: any;
+
+@Component({
+    templateUrl: '/app/services/components/service-events-grid.component.html',
+    providers: [
+        HttpModule,
+        RESTService
+    ]
+})
+export class ServiceEventsGrid implements OnInit {
+    @Input() settings: any;
+    deviceId: any;
+    data: wijmo.collections.CollectionView;
+    errorMessage: string;
+
+    constructor(private service: RESTService, private widgetService: WidgetService, private route: ActivatedRoute) { }
+
+    get pageSize(): number {
+        return this.data.pageSize;
+    }
+
+    set pageSize(value: number) {
+        if (this.data.pageSize != value) {
+            this.data.pageSize = value;
+            this.data.refresh();
+        }
+    }
+
+    ngOnInit() {
+        this.route.params.subscribe(params => {
+            this.deviceId = params['service'];
+
+        });
+        this.service.get('/dashboard/' + this.deviceId + '/notifications')
+            .subscribe(
+            (response) => {
+                this.data = new wijmo.collections.CollectionView(new wijmo.collections.ObservableArray(response.data));
+                this.data.pageSize = 10;
+            },
+            (error) => this.errorMessage = <any>error
+            );
+
+    }
+
+    getAccessColor(access: string) {
+
+        switch (access) {
+            case 'Allow':
+                return 'green';
+            case 'Blocked':
+                return 'red';
+            default:
+                return '';
+        }
+
+    }
+}
---
+++ VitalSignsWeb/VitalSigns.Web/scripts/services/components/service-events-tab.component.ts
@@ -0,0 +1,46 @@
+﻿import {Component, ComponentFactoryResolver, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+
+import {WidgetController, WidgetContainer, WidgetContract, WidgetService} from '../../core/widgets';
+import {AppNavigator} from '../../navigation/app.navigator.component';
+
+
+import {ServiceTab} from '../models/service-tab.interface';
+
+declare var injectSVG: any;
+declare var bootstrapNavigator: any;
+
+@Component({
+    templateUrl: '/app/services/components/service-events-tab.component.html',
+    providers: [WidgetService]
+})
+export class ServiceEventsTab extends WidgetController implements OnInit {
+    deviceId: any;
+    widgets: WidgetContract[];
+
+    constructor(protected resolver: ComponentFactoryResolver, protected widgetService: WidgetService, private route: ActivatedRoute) {
+        super(resolver, widgetService);
+    }
+
+    ngOnInit() {
+        this.route.params.subscribe(params => {
+            this.deviceId = params['service'];
+
+        });
+        this.widgets = [
+            {
+                id: 'ServiceEventsGrid',
+                title: 'Events',
+                name: 'ServiceEventsGrid',
+                css: 'col-xs-12',
+            }
+
+        ]
+        injectSVG();
+        bootstrapNavigator();
+
+    }
+
+}
+
+
