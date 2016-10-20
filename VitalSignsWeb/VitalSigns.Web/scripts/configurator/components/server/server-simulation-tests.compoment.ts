197b1ddb372aeb27ec72ea0218de115f1ade5a37
kiran <kiran@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-10-20 11:12:24 +0000

    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@870 6b605366-3ce6-ac4f-954a-f758cfb76b34

--- VitalSignsWeb/VitalSigns.Web/scripts/configurator/components/server/server-simulation-tests.compoment.ts
+++ VitalSignsWeb/VitalSigns.Web/scripts/configurator/components/server/server-simulation-tests.compoment.ts
@@ -1,6 +1,6 @@
 ﻿import {Component, OnInit} from '@angular/core';
 @Component({
-    selector: 'server-location-list',
+    selector: 'server-simulation-list',
     templateUrl: '/app/configurator/components/server/server-simulation-tests.compoment.html'
 
 })
