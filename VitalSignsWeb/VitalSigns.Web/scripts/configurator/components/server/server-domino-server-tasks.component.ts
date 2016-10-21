8d0ea36d34241dc64f73cc8d23a18d3a560d76ca
ometral <ometral@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-10-21 16:20:27 +0000

    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@911 6b605366-3ce6-ac4f-954a-f758cfb76b34

--- VitalSignsWeb/VitalSigns.Web/scripts/configurator/components/server/server-domino-server-tasks.component.ts
+++ VitalSignsWeb/VitalSigns.Web/scripts/configurator/components/server/server-domino-server-tasks.component.ts
@@ -1,6 +1,6 @@
 ﻿import {Component, OnInit, ViewChild, AfterViewInit, Input} from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {HTTP_PROVIDERS}    from '@angular/http';
+import {HttpModule}    from '@angular/http';
 import {RESTService} from '../../../core/services';
 import {GridBase} from '../../../core/gridBase';
 
@@ -14,18 +14,8 @@ import * as wjCoreModule from 'wijmo/wijmo.angular2.core';;
 
 @Component({
     templateUrl: '/app/configurator/components/server/server-domino-server-tasks.component.html',
-    directives: [
-        wjFlexGrid.WjFlexGrid,
-        wjFlexGrid.WjFlexGridColumn,
-        wjFlexGrid.WjFlexGridCellTemplate,
-        wjFlexGridFilter.WjFlexGridFilter,
-        wjFlexGridGroup.WjGroupPanel,
-        wjFlexInput.WjMenu,
-        wjFlexInput.WjMenuItem,
-        AppNavigator
-    ],
     providers: [
-        HTTP_PROVIDERS,
+        HttpModule,
         RESTService
     ]
 })
