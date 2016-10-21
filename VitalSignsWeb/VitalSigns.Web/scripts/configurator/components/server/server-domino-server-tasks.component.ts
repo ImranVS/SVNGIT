71baf1c506e98afd19c5b9b9569643651637655b
sowjanya <sowjanya@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-10-21 12:48:59 +0000

    servertask pages for configurator
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@901 6b605366-3ce6-ac4f-954a-f758cfb76b34

---
+++ VitalSignsWeb/VitalSigns.Web/scripts/configurator/components/server/server-domino-server-tasks.component.ts
@@ -0,0 +1,53 @@
+﻿import {Component, OnInit, ViewChild, AfterViewInit, Input} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {HTTP_PROVIDERS}    from '@angular/http';
+import {RESTService} from '../../../core/services';
+import {GridBase} from '../../../core/gridBase';
+
+import {AppNavigator} from '../../../navigation/app.navigator.component';
+import * as wjFlexGrid from 'wijmo/wijmo.angular2.grid';
+import * as wjFlexGridFilter from 'wijmo/wijmo.angular2.grid.filter';
+import * as wjFlexGridGroup from 'wijmo/wijmo.angular2.grid.grouppanel';
+import * as wjFlexInput from 'wijmo/wijmo.angular2.input';
+import * as wjCoreModule from 'wijmo/wijmo.angular2.core';;
+
+
+@Component({
+    templateUrl: '/app/configurator/components/server/server-domino-server-tasks.component.html',
+    directives: [
+        wjFlexGrid.WjFlexGrid,
+        wjFlexGrid.WjFlexGridColumn,
+        wjFlexGrid.WjFlexGridCellTemplate,
+        wjFlexGridFilter.WjFlexGridFilter,
+        wjFlexGridGroup.WjGroupPanel,
+        wjFlexInput.WjMenu,
+        wjFlexInput.WjMenuItem,
+        AppNavigator
+    ],
+    providers: [
+        HTTP_PROVIDERS,
+        RESTService
+    ]
+})
+export class ServerTasks extends GridBase {
+    selectedServers: string;
+    constructor(service: RESTService) {
+        super(service, '/Configurator/get_business_hours');
+        
+    }
+
+    ngOnInit() {
+        alert("hiiiiiiiiiiiii");
+    }
+    //saveBusinessHour() {
+    //    this.saveGridRow('/Configurator/save_business_hours');
+
+    //}
+
+    //delteBusinessHour() {
+    //    this.delteGridRow('/Configurator/delete_business_hours/');
+    //}
+}
+
+
+
