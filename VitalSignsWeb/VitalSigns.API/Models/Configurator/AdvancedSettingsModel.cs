56d103452408779b0a49b2a5969950bb79379601
durga <durga@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-10-24 07:29:25 +0000

    Advanced settings Model
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@921 6b605366-3ce6-ac4f-954a-f758cfb76b34

---
+++ VitalSignsWeb/VitalSigns.API/Models/Configurator/AdvancedSettingsModel.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace VitalSigns.API.Models
+{
+    public class AdvancedSettingsModel
+    {
+
+
+        [JsonProperty("memory_threshold")]
+        public int? MemoryThreshold { get; set; }
+
+        [JsonProperty("cpu_threshold")]
+        public int? CpuThreshold { get; set; }
+
+        [JsonProperty("server_days_alert")]
+        public int? ServerDaysAlert { get; set; }
+
+        [JsonProperty("cluster_replication_delay_threshold")]
+        public int? ClusterReplicationDelayThreshold { get; set; }
+
+
+        
+        [JsonProperty("proxy_server_type")]
+        public string ProxyServerType { get; set; }
+
+        [JsonProperty("proxy_server_protocol")]
+        public string ProxyServerprotocol { get; set; }
+
+        [JsonProperty("dbms_host_name")]
+        public string DbmsHostName { get; set; }
+        
+
+        [JsonProperty("dbms_name")]
+        public string DbmsName { get; set; }
+
+        [JsonProperty("dbms_port")]
+        public int? DbmsPort { get; set; }
+
+        [JsonProperty("collect_extended_statistics")]
+        public Boolean? CollectExtendedStatistics { get; set; }
+        
+      
+        public Boolean? collect_meeting_statistics { get; set; }
+       
+        [JsonProperty("extended_statistics_port")]
+        public int? ExtendedStatisticsPort { get; set; }
+        
+        [JsonProperty("meeting_host_name")]
+        public string MeetingHostName { get; set; }
+        
+        [JsonProperty("meeting_port")]
+        public int? MeetingPort { get; set; }
+      
+        [JsonProperty("meeting_require_ssl")]
+        public Boolean? MeetingRequireSSL { get; set; }
+        
+        [JsonProperty("conference_host_name")]
+        public string ConferenceHostName { get; set; }
+
+        [JsonProperty("conference_port")]
+        public int? ConferencePort { get; set; }
+        
+        [JsonProperty("conference_require_ssl")]
+        public Boolean? ConferenceRequireSSL { get; set; }
+        
+
+         [JsonProperty("database_settings_host_name")]
+        public string DatabaseSettingsHostName { get; set; }
+        
+        [JsonProperty("database_settings_credentials_id")]
+        public string DatabaseSettingsCredentialsId { get; set; }
+        
+       [JsonProperty("database_settings_port")]
+        public int? DatabaseSettingsPort { get; set; }
+    }
+}
