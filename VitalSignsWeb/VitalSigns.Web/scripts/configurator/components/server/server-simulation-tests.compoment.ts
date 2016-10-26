1e7907d1ca589ea36dc22356cb9ded9327f5e2aa
sowmya <sowmya@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-10-26 14:47:20 +0000

    simulationtests tab
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@954 6b605366-3ce6-ac4f-954a-f758cfb76b34

--- VitalSignsWeb/VitalSigns.Web/scripts/configurator/components/server/server-simulation-tests.compoment.ts
+++ VitalSignsWeb/VitalSigns.Web/scripts/configurator/components/server/server-simulation-tests.compoment.ts
@@ -1,9 +1,68 @@
-﻿import {Component, OnInit} from '@angular/core';
+﻿import {Component, OnInit, ViewChildren} from '@angular/core';
+import {FormBuilder, FormGroup, FormControl, Validators} from '@angular/forms';
+import {Router, ActivatedRoute} from '@angular/router';
+import {HttpModule}    from '@angular/http';
+
+import {RESTService} from '../../../core/services';
 @Component({
     selector: 'server-simulation-list',
-    templateUrl: '/app/configurator/components/server/server-simulation-tests.compoment.html'
-
+    templateUrl: '/app/configurator/components/server/server-simulation-tests.compoment.html',
+    providers: [
+        HttpModule,
+        RESTService
+        ]
 })
-export class SimulationTests  {
+export class SimulationTests implements OnInit {
+    @ViewChildren('name') inputName;
+    deviceId: any;
+    insertMode: boolean = false;
+    simulationTests: FormGroup;
+    errorMessage: string;
+    profileEmail: string;
+    formTitle: string;
 
+    constructor(
+        private formBuilder: FormBuilder,
+        private dataProvider: RESTService,
+        private router: Router,
+        private route: ActivatedRoute) {
+        this.simulationTests = this.formBuilder.group({
+            'id':[],
+            'create_activity_threshold': [],
+            'create_blog_threshold': [],
+            'create_bookmark_threshold': [],
+            'create_community_threshold': [],
+            'create_file_threshold': [],
+            'create_wiki_threshold': [],
+            'search_profile_threshold': [],
+            'create_activity': [false],
+            'create_blog': [false],
+            'create_bookmark': [false],
+            'create_community': [false],
+            'create_file': [false],
+            'create_wiki': [false],
+            'search_profile': [false]
+
+        });
+}
+
+    ngOnInit() {
+        this.route.params.subscribe(params => {
+            this.deviceId = params['service'];
+         });
+        this.dataProvider.get('/configurator/' + this.deviceId + '/get_simulationtests')
+            .subscribe(
+            response => {
+                console.log(response.data);
+                this.simulationTests.setValue(response.data);
+            },
+            (error) => this.errorMessage = <any>error
+            );
+    }
+    onSubmit(nameValue: any): void {
+        console.log(nameValue);
+        this.dataProvider.put('/configurator/save_simulationtests', nameValue)
+            .subscribe(
+            response => {});
+    }
 }
