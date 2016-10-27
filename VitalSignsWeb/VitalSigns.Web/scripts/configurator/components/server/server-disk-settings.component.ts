7f000b152981c9576ad205eb2404fff817ebcd14
durga <durga@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-10-27 09:14:13 +0000

    Server Disk Settings
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@968 6b605366-3ce6-ac4f-954a-f758cfb76b34

--- VitalSignsWeb/VitalSigns.Web/scripts/configurator/components/server/server-disk-settings.component.ts
+++ VitalSignsWeb/VitalSigns.Web/scripts/configurator/components/server/server-disk-settings.component.ts
@@ -25,7 +25,7 @@ export class DominoServerDiskSettings  implements OnInit {
     diskSettingsForm: FormGroup;
     selectedDiskSetting: any;
     selectedDiskSettingValue: any;
-    devices: string;
+   
     diskByPercentage: string;
     diskByGB: string;
     selectedDisks: string;
@@ -64,15 +64,99 @@ export class DominoServerDiskSettings  implements OnInit {
 
             }); 
     }
+    itemsSourceChangedHandler() {
+
+        var flex = this.flex;
+
+        if (flex) {
+            var colThresholdType = flex.columns.getColumn('threshold_type');
+
+            if (colThresholdType) {
+
+                colThresholdType.showDropDown = true; // or colors (just to show how)
+                var unitsData = [{ unit: "Percent", code: "Percent" }, { unit: "GB", code: "GB" }];
+
+                var unitsDataMap = new wijmo.grid.DataMap(unitsData, 'unit', 'code');
+
+                colThresholdType.dataMap = unitsDataMap;
+            }
+            else {
+                colThresholdType.dataMap = null;
+            }
+        }
+    }
     
     applySetting(nameValue: any){
      
       
-      
+        if (this.selectedDiskSetting == "allDisksBypercentage") {
+          
+            this.selectedDiskSettingValue = this.diskByPercentage;
+        }
+        else if (this.selectedDiskSetting == "allDisksByGB")
+            this.selectedDiskSettingValue = this.diskByGB;
+        else if (this.selectedDiskSetting == "selectedDisks") 
+        {
+            this.selectedDiskSettingValue = this.selectedDisks;
+            var slectedDiskSettingValues: DiskSttingsValue[] = [];
+            for (var _i = 0; _i < this.flex.collectionView.sourceCollection.length; _i++) {
+
+                var item = (<wijmo.collections.CollectionView>this.flex.collectionView.sourceCollection)[_i];
+
+                if (item.is_selected) {
+                    
+
+                    var dominoserverObject = new DiskSttingsValue();
+                    dominoserverObject.is_selected = item.is_selected;
+                    dominoserverObject.disk_name = item.disk_name;
+
+                    dominoserverObject.freespace_threshold = item.freespace_threshold;
+                    dominoserverObject.threshold_type = item.threshold_type;
+                   // alert(item.is_selected)
+                    slectedDiskSettingValues.push(dominoserverObject);
+                }
+            }
+            this.diskValues = slectedDiskSettingValues;
+        
+        }
+          
+        else if (this.selectedDiskSetting == "noDiskAlerts")
+            this.selectedDiskSettingValue = this.noDiskAlerts;
+       
+
+     
+        if (this.selectedDiskSetting == "selectedDisks") {
+
+            this.postData = {
+                "setting": this.selectedDiskSetting,
+                "value": this.diskValues,
+                "devices": this.deviceId
+            };
+           
+        }
+        else if (this.selectedDiskSetting == "noDiskAlerts")
+        {
+            this.postData = {
+                "setting": this.selectedDiskSetting,
+                "value": null,
+                "devices": this.deviceId
+            };
+
+        }
+        else {
+            this.postData = {
+                "setting": this.selectedDiskSetting,
+                "value": this.selectedDiskSettingValue,
+                "devices": this.deviceId
+            };
+        }
+        this.dataProvider.put('/Configurator/save_server_disk_settings', this.postData)
+            .subscribe(
+            response => {
+
+            });
     }
-    changeInDevices(server: string) {
-        this.devices = server;
-    }
+   
     get pageSize(): number {
         return this.data.pageSize;
     }
