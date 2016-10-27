8636162b08ea35beab2d53ca80bba4b396f982f2
sowjanya <sowjanya@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-10-27 10:22:11 +0000

    Servertasks pages
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@971 6b605366-3ce6-ac4f-954a-f758cfb76b34

--- VitalSignsWeb/VitalSigns.Web/scripts/configurator/components/server/server-domino-server-tasks.component.ts
+++ VitalSignsWeb/VitalSigns.Web/scripts/configurator/components/server/server-domino-server-tasks.component.ts
@@ -9,7 +9,9 @@ import * as wjFlexGrid from 'wijmo/wijmo.angular2.grid';
 import * as wjFlexGridFilter from 'wijmo/wijmo.angular2.grid.filter';
 import * as wjFlexGridGroup from 'wijmo/wijmo.angular2.grid.grouppanel';
 import * as wjFlexInput from 'wijmo/wijmo.angular2.input';
-import * as wjCoreModule from 'wijmo/wijmo.angular2.core';;
+import * as wjCoreModule from 'wijmo/wijmo.angular2.core';
+import {ActivatedRoute} from '@angular/router';
+import {FormBuilder, FormGroup, FormControl, Validators} from '@angular/forms';
 
 
 @Component({
@@ -21,22 +23,76 @@ import * as wjCoreModule from 'wijmo/wijmo.angular2.core';;
 })
 export class ServerTasks extends GridBase {
     selectedServers: string;
-    constructor(service: RESTService) {
-        super(service, '/Configurator/get_business_hours');
+    deviceId: any;
+    data: wijmo.collections.CollectionView;
+    TaskNames: any;
+    errorMessage: string;
+    currentForm: FormGroup;
+    selectedSettingValue: any;
+    selectedName: string;
+
+    constructor(service: RESTService, private formBuilder: FormBuilder, private route: ActivatedRoute) {
+        super(service, '/Configurator/get_server_tasks_info/57ace45abf46711cd4681e15');
+
+        this.currentForm = this.formBuilder.group({
+            'setting': [''],
+            'value': ['']
+           
+
+
+        });
+
+        this.service.get('/Configurator/get_tasks_names')
+            .subscribe(
+            (response) => {
+                this.TaskNames = response.data.TaskNames;
+
+            },
+            (error) => this.errorMessage = <any>error
+            );
+       
         
     }
 
     ngOnInit() {
-        alert("hiiiiiiiiiiiii");
+       
     }
-    //saveBusinessHour() {
-    //    this.saveGridRow('/Configurator/save_business_hours');
+    addServerTask(frmDialog: wijmo.input.Popup) {
+        this.addGridRow1(frmDialog);
+        this.currentEditItem.device_id = "57ace45abf46711cd4681e15";
 
-    //}
+    }
+    saveServerTasks(dlg: wijmo.input.Popup) {
+        //this.buildPostData("add", dlg);
+        this.selectedSettingValue = this.selectedName;
+        alert(this.selectedSettingValue);
 
-    //delteBusinessHour() {
-    //    this.delteGridRow('/Configurator/delete_business_hours/');
-    //}
+        this.saveGridRow1('/configurator/save_server_tasks', dlg);
+    }
+
+    buildPostData(setting: string, dlg) {
+        var postData = {
+            "setting": setting,
+          
+        };
+
+        console.log(postData);
+        this.currentForm.setValue(postData);
+        //this.saveGridRow1('/configurator/save_server_tasks', postData, dlg)
+        this.service.put('/Configurator/save_domino_server_tasks', postData)
+            .subscribe(
+            response => {
+
+            });
+
+    }
+
+
+
+   
+    delteServerTasks() {
+        this.delteGridRow('/Configurator/delete_server_tasks/57ace45abf46711cd4681e15/');
+    }
 }
 
 
