4e939bf93647a7ac938019cd17edfb451ae29be5
nshkarayeva <nshkarayeva@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-11-01 18:42:40 +0000

    Sample report.
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@1069 6b605366-3ce6-ac4f-954a-f758cfb76b34

--- VitalSignsWeb/VitalSigns.Web/scripts/reports/components/sample-report.component.ts
+++ VitalSignsWeb/VitalSigns.Web/scripts/reports/components/sample-report.component.ts
@@ -1,102 +1,124 @@
-﻿import {Component, ComponentFactoryResolver} from '@angular/core';
+﻿import {Component, ComponentFactoryResolver, OnInit} from '@angular/core';
 
 import {WidgetController, WidgetContract, WidgetService} from '../../core/widgets';
 
+import {RESTService} from '../../core/services/rest.service';
+
+declare var injectSVG: any;
+declare var bootstrapNavigator: any;
+
+
 @Component({
     templateUrl: '/app/reports/components/sample-report.component.html',
-    providers: [WidgetService]
+    providers: [
+        WidgetService,
+        RESTService
+    ]
 })
 export class SampleReport extends WidgetController {
+    contextMenuSiteMap: any;
+    widgets: WidgetContract[];
 
-    widgets: WidgetContract[] = [
-        {
-            id: 'mobileDevicesChart',
-            title: 'Mobile devices',
-            name: 'ChartComponent',
-            settings: {
-                url: '/DashBoard/mobile_user_devices/count_by_type',
-                chart: {
-                    chart: {
-                        renderTo: 'mobileDevicesChart',
-                        type: 'pie',
-                        height: 300
-                    },
-                    title: { text: '' },
-                    subtitle: { text: '' },
-                    xAxis: {
-                        categories: []
-                    },
-                    yAxis: {
-                        min: 0,
-                        endOnTick: false,
-                        allowDecimals: false,
-                        title: {
-                            enabled: false
-                        }
-                    },
-                    legend: {
-                        enabled: false
-                    },
-                    credits: {
-                        enabled: false
-                    },
-                    exporting: {
-                        enabled: false
-                    },
-                    series: []
-                }
-            }
-        },
-        {
-            id: 'mobileDevicesOSChart',
-            title: 'Mobile devices OS for all Servers',
-            name: 'ChartComponent',
-            settings: {
-                url: '/DashBoard/mobile_user_devices/count_by_os',
-                chart: {
-                    chart: {
-                        renderTo: 'mobileDevicesOSChart',
-                        type: 'pie',
-                        height: 300
-                    },
-                    title: { text: '' },
-                    subtitle: { text: '' },
-                    xAxis: {
-                        categories: []
-                    },
-                    yAxis: {
-                        min: 0,
-                        endOnTick: false,
-                        allowDecimals: false,
-                        title: {
-                            enabled: false
-                        }
-                    },
-                    legend: {
-                        enabled: false
-                    },
-                    credits: {
-                        enabled: false
-                    },
-                    exporting: {
-                        enabled: false
-                    },
-                    series: []
-                }
-            }
-        },
-        {
-            id: 'mobileUsersTable',
-            title: 'Mobile users',
-            name: 'MobileUsers',
-            settings: {}
-        }
-    ]
-
-    constructor(protected resolver: ComponentFactoryResolver, protected widgetService: WidgetService) {
+    constructor(protected resolver: ComponentFactoryResolver, protected widgetService: WidgetService, private service: RESTService) {
 
         super(resolver, widgetService);
 
     }
 
+    ngOnInit() {
+
+        this.service.get('/navigation/sitemaps/traveler_reports')
+            .subscribe
+            (
+            data => this.contextMenuSiteMap = data,
+            error => console.log(error)
+            );
+        this.widgets = [
+            {
+                id: 'mobileDevicesChart',
+                title: 'Mobile devices',
+                name: 'ChartComponent',
+                settings: {
+                    url: '/DashBoard/mobile_user_devices/count_by_type',
+                    chart: {
+                        chart: {
+                            renderTo: 'mobileDevicesChart',
+                            type: 'pie',
+                            height: 300
+                        },
+                        title: { text: '' },
+                        subtitle: { text: '' },
+                        xAxis: {
+                            categories: []
+                        },
+                        yAxis: {
+                            min: 0,
+                            endOnTick: false,
+                            allowDecimals: false,
+                            title: {
+                                enabled: false
+                            }
+                        },
+                        legend: {
+                            enabled: false
+                        },
+                        credits: {
+                            enabled: false
+                        },
+                        exporting: {
+                            enabled: false
+                        },
+                        series: []
+                    }
+                }
+            },
+            {
+                id: 'mobileDevicesOSChart',
+                title: 'Mobile devices OS for all Servers',
+                name: 'ChartComponent',
+                settings: {
+                    url: '/DashBoard/mobile_user_devices/count_by_os',
+                    chart: {
+                        chart: {
+                            renderTo: 'mobileDevicesOSChart',
+                            type: 'pie',
+                            height: 300
+                        },
+                        title: { text: '' },
+                        subtitle: { text: '' },
+                        xAxis: {
+                            categories: []
+                        },
+                        yAxis: {
+                            min: 0,
+                            endOnTick: false,
+                            allowDecimals: false,
+                            title: {
+                                enabled: false
+                            }
+                        },
+                        legend: {
+                            enabled: false
+                        },
+                        credits: {
+                            enabled: false
+                        },
+                        exporting: {
+                            enabled: false
+                        },
+                        series: []
+                    }
+                }
+            },
+            {
+                id: 'mobileUsersTable',
+                title: 'Mobile users',
+                name: 'MobileUsers',
+                settings: {}
+            }
+        ];
+        injectSVG();
+        bootstrapNavigator();
+
+    }
 }
