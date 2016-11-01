67c094c1e234e1debd5c67a3f89625b9e40b7a11
sowmya <sowmya@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-11-01 08:52:28 +0000

    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@1053 6b605366-3ce6-ac4f-954a-f758cfb76b34

---
+++ VitalSignsWeb/VitalSigns.API/Models/Configurator/AlertsModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace VitalSigns.API.Models
+{
+    public class AlertsModel
+    {
+        [JsonProperty ("id")]
+        public string ID { get; set; }
+
+        [JsonProperty("enable_persitent_alerting")]
+        public bool EnablePersitentAlerting { get; set; }
+
+        [JsonProperty("alert_interval")]
+        public int AlertInterval { get; set; }
+
+        [JsonProperty("alert_duration")]
+        public int AlertDuration { get; set; }
+
+        [JsonProperty("e_mail")]
+        public string EMail { get; set; }
+
+        [JsonProperty("enable_alert_limits")]
+        public bool EnableAlertLimits { get; set; }
+
+        [JsonProperty("total_maximum_alerts_per_definition")]
+        public int TotalMaximumAlertsPerDefinition { get; set; }
+
+        [JsonProperty ("total_maximum_alerts_per_day")]
+        public int TotalMaximumAlertsPerDay { get; set; }
+
+        [JsonProperty("enable_SNMP_traps")]
+        public bool EnableSNMPTraps { get; set; }
+
+        [JsonProperty("host_name")]
+        public string HostName { get; set; }
+
+        [JsonProperty("device_name")]
+        public string DeviceName { get; set; }
+
+        [JsonProperty ("device_type")]
+        public string DeviceType { get; set; }
+
+        [JsonProperty("alert_type")]
+        public string AlertType { get; set; }
+
+        [JsonProperty("location")]
+        public string Location { get; set; }
+
+        [JsonProperty("event_detected_sent")]
+        public DateTime? EventDetectedSent { get; set; }
+
+        [JsonProperty("event_dismissed")]
+        public DateTime? EventDismissed { get; set; }
+
+        [JsonProperty("notification_sent_to")]
+        public string NotificationSentTo { get; set; }
+
+        [JsonProperty("details")]
+        public string Details { get; set; }
+    }
+}
