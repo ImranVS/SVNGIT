43ae2752bb9cfcaa27724948ab77aef0d6c40bfc
sowjanya <sowjanya@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-11-02 14:22:46 +0000

    IBM Domino task
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@1092 6b605366-3ce6-ac4f-954a-f758cfb76b34

---
+++ VitalSignsWeb/VitalSigns.API/Models/Configurator/ServerTaskDefinitionModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace VitalSigns.API.Models
+{
+    public class ServerTaskDefinitionModel
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("task_name")]
+        public string TaskName { get; set; }
+
+        [JsonProperty("load_string")]
+        public string LoadString { get; set; }
+
+        [JsonProperty("console_string")]
+        public string ConsoleString { get; set; }
+
+        [JsonProperty("freeze_detect")]
+        public bool FreezeDetect { get; set; }
+
+        [JsonProperty("idle_string")]
+        public string IdleString { get; set; }
+
+        [JsonProperty("max_busy_time")]
+        public int MaxBusyTime { get; set; }
+
+        [JsonProperty("retry_count")]
+        public int RetryCount { get; set; }
+    }
+}
