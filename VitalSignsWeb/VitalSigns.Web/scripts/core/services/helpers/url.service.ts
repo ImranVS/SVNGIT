c796ad3d609797dcc2eeb6d2ec761e516145ba79
ometral <ometral@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-11-03 22:55:38 +0000

    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@1147 6b605366-3ce6-ac4f-954a-f758cfb76b34

---
+++ VitalSignsWeb/VitalSigns.Web/scripts/core/services/helpers/url.service.ts
@@ -0,0 +1,41 @@
+﻿import {Injectable}     from '@angular/core';
+
+@Injectable()
+export class UrlHelperService {
+
+    public getUrlPath(rawUrl: string): string {
+
+        let regex = /[^\?]*/;
+
+        return regex.exec(rawUrl)[0];
+
+    }
+
+    public getUrlQueryParams(rawUrl: string): any {
+
+        let paramsMatcher = /[^\?]*\?(.*)/;
+
+        if (paramsMatcher.test(rawUrl)) {
+
+            let params = paramsMatcher.exec(rawUrl)[1];
+
+            let paramMatcher = /\&?([^\=]*)\=([^\&]*)/g;
+
+            let queryParams = {};
+            let match = paramMatcher.exec(params);
+
+            while (match != null) {
+
+                queryParams[match[1]] = match[2];
+                match = paramMatcher.exec(params);
+
+            }
+
+            return queryParams;
+        }
+        else
+            return {};
+
+    }
+
+}
