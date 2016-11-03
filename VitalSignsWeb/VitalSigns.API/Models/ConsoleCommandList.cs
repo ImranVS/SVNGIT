17b732a6c6b50ef65149c3d00fba90794a681272
dhanraj <dhanraj@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-11-03 18:31:18 +0000

    VSPLUS-3266
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@1141 6b605366-3ce6-ac4f-954a-f758cfb76b34

---
+++ VitalSignsWeb/VitalSigns.API/Models/ConsoleCommandList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace VitalSigns.API.Models
+{
+    public class ConsoleCommandList
+    {
+        [JsonProperty("server_name")]
+        public string DeviceName { get; set; }
+
+        [JsonProperty("command")]
+        public string Command { get; set; }
+
+        [JsonProperty("submitter")]
+        public string Submitter { get; set; }
+
+        [JsonProperty("result")]
+        public string Result { get; set; }
+
+        [JsonProperty("comment")]
+        public string Comment { get; set; }
+
+       
+    }
+}
