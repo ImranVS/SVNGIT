09a9950923dfaa7dd64d9e225fb878a1f0324d9a
dhanraj <dhanraj@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-11-03 18:13:29 +0000

    VSPLUS-3266
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@1138 6b605366-3ce6-ac4f-954a-f758cfb76b34

---
+++ VitalSignsWeb/VitalSigns.Web/scripts/widgets/mobile-users/components/console-commands-list.component.ts
@@ -0,0 +1,36 @@
+﻿import {Component, Input, OnInit} from '@angular/core';
+import {HttpModule}    from '@angular/http';
+
+import {WidgetComponent} from '../../../core/widgets';
+import {RESTService} from '../../../core/services';
+
+import {ConsoleCommand} from '../models/console-command';
+
+@Component({
+    templateUrl: './app/widgets/mobile-users/components/console-commands-list.component.html',
+    providers: [
+        HttpModule,
+        RESTService
+    ]
+})
+export class ConsoleCommands implements WidgetComponent, OnInit {
+    @Input() settings: any;
+
+    errorMessage: string;
+
+    consoleCommands: any;
+
+    constructor(private service: RESTService) { }
+
+    loadData() {
+        this.service.get('/dashboard/console_command_list')
+            .subscribe(
+            data => this.consoleCommands = data.data,
+            error => this.errorMessage = <any>error
+            );
+    }
+
+    ngOnInit() {
+        this.loadData();
+    }
+}
