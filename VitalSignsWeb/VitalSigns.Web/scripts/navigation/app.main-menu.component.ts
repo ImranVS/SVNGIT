e964bb31371d3e0134c2f41c7c56b79aaaeb52be
ometral <ometral@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-11-03 21:53:18 +0000

    IMP: router links now support query params in left menu
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@1145 6b605366-3ce6-ac4f-954a-f758cfb76b34

--- VitalSignsWeb/VitalSigns.Web/scripts/navigation/app.main-menu.component.ts
+++ VitalSignsWeb/VitalSigns.Web/scripts/navigation/app.main-menu.component.ts
@@ -4,4 +4,41 @@
     selector: 'app-main-menu',
     templateUrl: '/partial/sitemap',
 })
-export class AppMainMenu { }
+export class AppMainMenu {
+
+    getUrlPath(rawUrl: string): string {
+    
+        let regex = /[^\?]*/;
+
+        return regex.exec(rawUrl)[0];
+
+    }
+
+    getUrlQueryParams(rawUrl: string): any {
+    
+        let paramsMatcher = /[^\?]*\?(.*)/;
+        
+        if (paramsMatcher.test(rawUrl)) {
+        
+            let params = paramsMatcher.exec(rawUrl)[1];
+
+            let paramMatcher = /\&?([^\=]*)\=([^\&]*)/g;
+            
+            let queryParams = {};
+            let match = paramMatcher.exec(params);
+
+            while (match != null) {
+
+                queryParams[match[1]] = match[2];
+                match = paramMatcher.exec(params);
+
+            }
+            
+            return queryParams;
+        }
+        else
+            return {};
+
+    }
+
+}
