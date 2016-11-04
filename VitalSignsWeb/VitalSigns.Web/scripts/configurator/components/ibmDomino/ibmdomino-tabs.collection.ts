a60c5db93ee23b6bfbe8ad3ab3584761ec685be8
sowmya <sowmya@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-11-04 15:08:45 +0000

    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@1161 6b605366-3ce6-ac4f-954a-f758cfb76b34

--- VitalSignsWeb/VitalSigns.Web/scripts/configurator/components/ibmDomino/ibmdomino-tabs.collection.ts
+++ VitalSignsWeb/VitalSigns.Web/scripts/configurator/components/ibmDomino/ibmdomino-tabs.collection.ts
@@ -4,5 +4,6 @@ export * from './Ibm-notes-database-replicas.component';
 export * from './Ibm-notes-databases.component';
 export * from './Ibm-domino-settings.component';
 export * from './Ibm-travelerdatastore.component';
+export * from './Ibm-custom-statistics.component';
 export * from './Ibm-domino-log-file-scanning.component';
 export * from './Ibm-save-domino-log-file-scanning.component';
