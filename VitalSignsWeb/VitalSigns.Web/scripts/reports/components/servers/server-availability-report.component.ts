49bd6b2600b7d36727659746e3e63a75f6eb0c31
wstanulis <wstanulis@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-11-07 21:21:15 +0000

    VSPLUS-3268
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@1208 6b605366-3ce6-ac4f-954a-f758cfb76b34

---
+++ VitalSignsWeb/VitalSigns.Web/scripts/reports/components/servers/server-availability-report.component.ts
@@ -0,0 +1,103 @@
+﻿import {Component, ComponentFactoryResolver, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {WidgetController, WidgetContract, WidgetService} from '../../../core/widgets';
+import {RESTService} from '../../../core/services/rest.service';
+
+import * as helpers from '../../../core/services/helpers/helpers';
+
+declare var injectSVG: any;
+declare var bootstrapNavigator: any;
+
+
+@Component({
+    templateUrl: '/app/reports/components/servers/server-availability-report.component.html',
+    providers: [
+        WidgetService,
+        RESTService,
+        helpers.UrlHelperService
+    ]
+})
+export class ServerAvailabilityReport extends WidgetController {
+    contextMenuSiteMap: any;
+    widgets: WidgetContract[];
+    statname: string;
+    title: string;
+
+    constructor(protected resolver: ComponentFactoryResolver, protected widgetService: WidgetService, private service: RESTService, private route: ActivatedRoute,
+        protected urlHelpers: helpers.UrlHelperService) {
+
+        super(resolver, widgetService);
+
+    }
+
+    ngOnInit() {
+
+        this.route.queryParams.subscribe(params => {
+            this.statname = params['statname'];
+            this.title = params['title'];
+        });
+
+        this.service.get('/navigation/sitemaps/server_reports')
+            .subscribe
+            (
+            data => this.contextMenuSiteMap = data,
+            error => console.log(error)
+            );
+        this.widgets = [
+            {
+                id: 'blogs',
+                title: 'Response Times',
+                name: 'ChartComponent',
+                css: 'col-xs-12 col-sm-6 col-md-6 col-lg-4',
+                settings: {
+                    url: `/reports/server_availability?statName=${this.statname}`,
+                    chart: {
+                        chart: {
+                            renderTo: 'blogs',
+                            type: 'bar',
+                            height: 240
+                        },
+                        title: { text: '' },
+                        subtitle: { text: '' },
+                        xAxis: {
+                            categories: []
+                        },
+                        yAxis: {
+                            min: 0,
+                            endOnTick: false,
+                            allowDecimals: false,
+                            title: {
+                                enabled: false
+                            }
+                        },
+                        plotOptions: {
+                            bar: {
+                                dataLabels: {
+                                    enabled: false
+                                },
+                                groupPadding: 0.1,
+                                borderWidth: 0
+                            },
+                            series: {
+                                pointPadding: 0
+                            }
+                        },
+                        legend: {
+                            enabled: false
+                        },
+                        credits: {
+                            enabled: false
+                        },
+                        exporting: {
+                            enabled: false
+                        },
+                        series: []
+                    }
+                }
+            },
+        ];
+        injectSVG();
+        bootstrapNavigator();
+
+    }
+}
