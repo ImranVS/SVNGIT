1dad04bdf30eb79a273a643f6a9804300bdbb849
durga <durga@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-11-08 11:07:07 +0000

    Mail Delivery Status
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@1218 6b605366-3ce6-ac4f-954a-f758cfb76b34

---
+++ VitalSignsWeb/VitalSigns.API/Models/Dashboard/MailDeliverStatusModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace VitalSigns.API.Models
+{
+    public class MailDeliveryStatusModel
+    {
+        [JsonProperty("device_name")]
+        public string DeviceName { get; set; }
+
+
+        [JsonProperty("pending_mail")]
+        public int? PendingMail { get; set; }
+
+        [JsonProperty("dead_mail")]
+        public int? DeadMail { get; set; }
+
+        [JsonProperty("held_mail")]
+        public int? HeldMail { get; set; }
+
+        [JsonProperty("location")]
+        public string Location { get; set; }
+
+        [JsonProperty("category")]
+        public string Category { get; set; }
+
+        [JsonProperty("last_updated")]
+        public DateTime? LastUpdated { get; set; }
+     
+        [JsonProperty("StatusCode")]
+        public string StatusCode { get; set; }
+        
+    }
+}
