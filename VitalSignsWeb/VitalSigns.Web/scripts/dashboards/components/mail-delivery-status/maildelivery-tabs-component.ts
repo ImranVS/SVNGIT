2a0d6340f88445447ba104cf49601bfacced6a3b
durga <durga@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-11-08 07:36:36 +0000

    Mail Delivery Status
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@1211 6b605366-3ce6-ac4f-954a-f758cfb76b34

---
+++ VitalSignsWeb/VitalSigns.Web/scripts/dashboards/components/mail-delivery-status/maildelivery-tabs-component.ts
@@ -0,0 +1,51 @@
+﻿/// <reference path="maildelivery-tabs.collection.ts" />
+import {Component, OnInit, ComponentFactoryResolver, ComponentFactory, ElementRef, ComponentRef, ViewChild, ViewContainerRef} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {HttpModule}    from '@angular/http';
+
+import {RESTService} from '../../../core/services';
+
+import * as ServiceTabs from './maildelivery-tabs.collection';
+
+@Component({
+    templateUrl: '/app/dashboards/components/mail-delivery-status/maildelivery-tabs-component.html',
+    providers: [
+        HttpModule,
+        RESTService
+    ]
+})
+export class MailDeliveryStatus implements OnInit {
+    @ViewChild('tab', { read: ViewContainerRef }) target: ViewContainerRef;
+
+    tabsData: any;
+    activeTabComponent: ComponentRef<{}>;
+    constructor(private resolver: ComponentFactoryResolver, private elementRef: ElementRef) { }
+    selectTab(tab: any) {
+  
+        this.tabsData.forEach(tab => tab.active = false);
+        tab.active = true;
+       
+        if (this.activeTabComponent)
+            this.activeTabComponent.destroy();
+        let factory = this.resolver.resolveComponentFactory(ServiceTabs[tab.component]);
+        this.activeTabComponent = this.target.createComponent(factory);
+    }
+    ngOnInit() {
+        this.tabsData = [
+            {
+                "title": "Domino",
+                "component": "DominoMailDeliveryStatus",
+                "path": "/app/dashboards/components/mail-delivery-status/domino-mail-delivery-status.component'",
+                "active": false
+            }
+            ,
+            {
+                "title": "Exchange",
+                "component": "ExchangeMailDeliveryStatus",
+                "path": "/app/dashboards/components/mail-delivery-status/exchange-mail-delivery-status.component'",
+                "active": false
+            }
+        ];
+        this.selectTab(this.tabsData[0]);
+    };
+}
