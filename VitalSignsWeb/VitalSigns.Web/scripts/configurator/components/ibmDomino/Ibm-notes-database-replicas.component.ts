0ceb4049106cdfca16c614e906abf33bb1630916
durga <durga@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-11-08 12:10:10 +0000

    Fixing Popup Issue
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@1226 6b605366-3ce6-ac4f-954a-f758cfb76b34

--- VitalSignsWeb/VitalSigns.Web/scripts/configurator/components/ibmDomino/Ibm-notes-database-replicas.component.ts
+++ VitalSignsWeb/VitalSigns.Web/scripts/configurator/components/ibmDomino/Ibm-notes-database-replicas.component.ts
@@ -10,7 +10,7 @@ import {GridBase} from '../../../core/gridBase';
 export class NotesDatabaseReplica extends GridBase implements OnInit {
     sererNames: any;
     errorMessage: any;
-
+    currentEditItem: any;
     constructor(service: RESTService) {
         super(service);
         this.formName = "Notes Databse Replica";
@@ -18,8 +18,10 @@ export class NotesDatabaseReplica extends GridBase implements OnInit {
             .subscribe(
             (response) => {
                 this.sererNames = response.data.serversData;
+            
 
-
+               
+                console.log(this.sererNames);
             },
             (error) => this.errorMessage = <any>error
         );
@@ -35,7 +37,26 @@ export class NotesDatabaseReplica extends GridBase implements OnInit {
         this.delteGridRow('/configurator/notes_database_replica/');
     }
   
+   
 
+    addNotesDatabaseReplica(dlg: wijmo.input.Popup) {
+        this.addGridRow(dlg);
+        this.currentEditItem.device_name = "";
+        this.currentEditItem.is_enabled = "";
+        this.currentEditItem.scan_interval = "";
+        this.currentEditItem.off_hours_scan_interval = "";
+        this.currentEditItem.domino_server_a  = "";
+        this.currentEditItem.domino_server_a_file_mask = "";
+        this.currentEditItem.domino_server_a_exclude_folders = "";
+        this.currentEditItem.domino_server_b = "";
+        this.currentEditItem.domino_server_b_file_mask = "";
+        this.currentEditItem.domino_server_b_exclude_folders = "";
+        this.currentEditItem.domino_server_c = "";
+        this.currentEditItem.domino_server_c_file_mask = "";
+        this.currentEditItem.domino_server_c_exclude_folders = "";
+        this.currentEditItem.difference_threshold = "";
+        
+    }
 }
 
 
