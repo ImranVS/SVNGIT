e6a7c31b17da3778f34f3167a6bc614e553aa6d0
durga <durga@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-11-09 10:44:13 +0000

    Mobile Users
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@1243 6b605366-3ce6-ac4f-954a-f758cfb76b34

---
+++ VitalSignsWeb/VitalSigns.Web/scripts/configurator/components/mobileusers/mobile-users.component.ts
@@ -0,0 +1,34 @@
+﻿import {Component, OnInit, ViewChild, AfterViewInit} from '@angular/core';
+import {RESTService} from '../../../core/services';
+import {GridBase} from '../../../core/gridBase';
+
+
+@Component({
+    templateUrl: '/app/configurator/components/mobileusers/mobile-users.component.html',
+    providers: [RESTService]
+})
+export class MobileUser extends GridBase implements OnInit {
+    sererNames: any;
+    errorMessage: any;
+    currentEditItem: any;
+    constructor(service: RESTService) {
+        super(service);
+        this.formName = "Critical Devices";
+       
+      
+    }
+    ngOnInit() {
+        this.initialGridBind('/configurator/get_mobile_users');
+    }
+    saveMobileUsers(dlg: wijmo.input.Popup) {      
+        this.saveGridRow('/configurator/save_mobileusers', dlg);
+    }
+    delteMobileUsers() {
+        this.delteGridRow('/configurator/delete_mobile_users/');
+    }
+  
+ 
+}
+
+
+
