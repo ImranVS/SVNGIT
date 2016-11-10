928fa788ea1887eb2ca109b2b9eca2298366f8b5
durga <durga@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-11-10 14:29:05 +0000

    VSPLUS-3289 Configurator: Mobile Devices/Key Users
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@1295 6b605366-3ce6-ac4f-954a-f758cfb76b34

--- VitalSignsWeb/VitalSigns.Web/scripts/configurator/components/mobileusers/mobile-users.component.ts
+++ VitalSignsWeb/VitalSigns.Web/scripts/configurator/components/mobileusers/mobile-users.component.ts
@@ -11,14 +11,25 @@ export class MobileUser extends GridBase implements OnInit {
     sererNames: any;
     errorMessage: any;
     currentEditItem: any;
+    mobileDeviceData: wijmo.collections.CollectionView;
+    
     constructor(service: RESTService) {
+  
         super(service);
         this.formName = "Critical Devices";
-       
+        
       
     }
     ngOnInit() {
         this.initialGridBind('/configurator/get_mobile_users');
+        this.service.get('/configurator/get_all_mobile_devices')
+            .subscribe(
+            (response) => {
+                this.mobileDeviceData = new wijmo.collections.CollectionView(new wijmo.collections.ObservableArray(response.data));
+                this.mobileDeviceData.pageSize = 10;
+            },
+            (error) => this.errorMessage = <any>error
+            );
     }
     saveMobileUsers(dlg: wijmo.input.Popup) {      
         this.saveGridRow('/configurator/save_mobileusers', dlg);
@@ -26,7 +37,22 @@ export class MobileUser extends GridBase implements OnInit {
     delteMobileUsers() {
         this.delteGridRow('/configurator/delete_mobile_users/');
     }
-  
+    get MobileDevicepageSize(): number {
+        return this.mobileDeviceData.pageSize;
+    }
+    set MobileDevicepageSize(value: number) {
+
+        if (this.mobileDeviceGrid)
+        {
+            if (this.mobileDeviceData.pageSize != value) {
+                this.mobileDeviceData.pageSize = value;
+                if (this.mobileDeviceGrid) {
+                    (<wijmo.collections.IPagedCollectionView>this.mobileDeviceGrid.collectionView).pageSize = value;
+                }
+            }
+        }
+       
+    }
  
 }
 
