660367299d680e54bbfdf38d2b5f37b23dd33c40
nshkarayeva <nshkarayeva@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-11-11 22:31:48 +0000

    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@1327 6b605366-3ce6-ac4f-954a-f758cfb76b34

---
+++ VitalSignsWeb/VitalSigns.API/Models/Configurator/NotificationsModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace VitalSigns.API.Models
+{
+    public class NotificationsModel
+    {
+        [JsonProperty ("id")]
+        public string ID { get; set; }
+
+        [JsonProperty("notification_name")]
+        public string NotificationName { get; set; }
+
+        [JsonProperty("interval")]
+        public string Interval { get; set; }
+
+        [JsonProperty("business_hours_type")]
+        public string BusinessHoursType { get; set; }
+
+        [JsonProperty("b_id")]
+        public int BId { get; set; }
+
+        [JsonProperty("start_time")]
+        public string StartTime { get; set; }
+
+        [JsonProperty("duration")]
+        public int Duration { get; set; }
+
+        [JsonProperty("days")]
+        public string Days { get; set; }
+
+        [JsonProperty("send_via")]
+        public string SendVia { get; set; }
+
+        [JsonProperty("send_to")]
+        public string SendTo { get; set; }
+
+        [JsonProperty("copy_to")]
+        public string CopyTo { get; set; }
+
+        [JsonProperty("blind_copy_to")]
+        public string BlindCopyTo { get; set; }
+
+        [JsonProperty("persistent_notification")]
+        public bool PersistentNotification { get; set; }
+
+        [JsonProperty("escalation")]
+        public string Escalation { get; set; }
+
+    }
+
+    public class NotificationDefinition
+    {
+        [JsonProperty("notifications")]
+        public NotificationsModel Notifications { get; set; }
+
+        [JsonProperty("selected_events")]
+        public List<EventsModel> SelectedEvents { get; set; }
+
+        [JsonProperty("selected_servers")]
+        public List<ServersModel> SelectedServers { get; set; }
+    }
+}
