9853cf9802a0ff0d0e592028fe44e5148dcf0007
sowjanya <sowjanya@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-11-11 10:42:45 +0000

    Maintenance changes
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@1306 6b605366-3ce6-ac4f-954a-f758cfb76b34

--- VitalSignsWeb/VitalSigns.Web/scripts/configurator/components/applicationSettings/application-settings-maintenance.component.ts
+++ VitalSignsWeb/VitalSigns.Web/scripts/configurator/components/applicationSettings/application-settings-maintenance.component.ts
@@ -1,4 +1,4 @@
-﻿import {Component, OnInit} from '@angular/core';
+﻿import {Component, OnInit, ViewChild} from '@angular/core';
 import {HttpModule}    from '@angular/http';
 import {RESTService} from '../../../core/services';
 import {GridBase} from '../../../core/gridBase';
@@ -12,9 +12,11 @@ import {GridBase} from '../../../core/gridBase';
     ]
 })
 export class Maintenance extends GridBase implements OnInit  {  
-   devices: string;
-    errorMessage: string;
-    dataMobileUsers: wijmo.collections.CollectionView;
+    devices: string;
+    
+   errorMessage: string;
+   dataMobileUsers: wijmo.collections.CollectionView;
+ 
     selectedSetting: string="1";
     durationSetting: any;
     selectedSettingValue: any;
@@ -24,7 +26,8 @@ export class Maintenance extends GridBase implements OnInit  {
     repeat_monthly: any;
     end_date: any;
     day_of_the_month: any;
-    i: any;
+    splittedVlue: string;
+   
     //currentDeviceType: string = ""
     
    
@@ -58,39 +61,54 @@ export class Maintenance extends GridBase implements OnInit  {
 
     ngOnInit() {
         this.initialGridBind('/Configurator/get_maintenance');
+    }
 
+    keyUsersGridBind() {
         this.service.get('/Dashboard/mobile_user_devices')
             .subscribe(
             (response) => {
-                this.dataMobileUsers = new wijmo.collections.CollectionView(new wijmo.collections.ObservableArray(response.data));
+                console.log("Key Users :" + this.keyUsers);
+                var resultData: any = [];
+                for (var item of response.data) {
+                    if (this.keyUsers) {
+                        var value = this.keyUsers.filter((record) => record==item.id);
+                        console.log(item.id);
+                        console.log(value);
+                        if (value.length > 0) {
+                            item.is_selected = true;
+                        }
+                    }
+                    resultData.push(item);
+                }
+
+                console.log(resultData);
+                this.dataMobileUsers = new wijmo.collections.CollectionView(new wijmo.collections.ObservableArray(resultData));
                 this.dataMobileUsers.pageSize = 10;
             },
             (error) => this.errorMessage = <any>error
             );
-
-      
-
     }
+    editMaintenance(dlg: wijmo.input.Popup) {
+        this.editGridRow(dlg);
+
+        var selectedWeekDays = this.currentEditItem.maintenance_days_list;
+       // alert(selectedWeekDays);
+        console.log(selectedWeekDays);
+        this.splittedVlue = selectedWeekDays.split(":");
+        //alert(this.splittedVlue);
+        console.log(this.splittedVlue);
+        this.selectedSetting = this.currentEditItem.maintain_type_value;
+        console.log(this.selectedSetting);
+       this.durationSetting = this.currentEditItem.duration_type;
+       this.keyUsers = this.currentEditItem.key_users ;
+       this.devices = this.currentEditItem.device_list;
+       this.keyUsersGridBind();
+    }
+
+
     saveMaintenance(dlg: wijmo.input.Popup) {
         var selectedWeekDays = "";
-
-       
-       
-        //this.weekDays[0] == true
-        //alert(this.weekDays.weekday)
         this.currentEditItem.maintain_type = this.selectedSetting;
-      
-
-        //if (this.selectedSetting == "2" || "3" || "4") {
-
-        //    if (this.durationSetting == "1")
-        //        this.currentEditItem.end_date = this.end_date;
-        //    else
-        //        this.currentEditItem.end_date = "";
-           
-        //}
-
-       
         if (this.selectedSetting == "3") {
 
             for (var item of this.weekDays) {
@@ -181,6 +199,19 @@ export class Maintenance extends GridBase implements OnInit  {
 
         this.saveGridRow('/Configurator/save_maintenancedata',dlg);  
     }
+
+    addMaintenance(dlg: wijmo.input.Popup) {
+        this.addGridRow(dlg);
+        this.currentEditItem.name = "";
+        this.currentEditItem.start_date = "";
+        this.currentEditItem.end_date = "";
+        this.currentEditItem.start_time = "";
+        this.currentEditItem.end_time = "";
+        this.currentEditItem.duration = "";
+       
+
+    }
+
     deleteMaintenance() {      
         this.delteGridRow('/Configurator/delete_maintenancedata/');  
     }
