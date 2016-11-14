8ad25dbde02f15f555da66336d54752cf186ec93
sowmya <sowmya@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-11-14 11:10:06 +0000

    overall sametime
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@1350 6b605366-3ce6-ac4f-954a-f758cfb76b34

---
+++ VitalSignsWeb/VitalSigns.Web/scripts/configurator/components/ibmDomino/Ibm-overall-sametime-statistics.component.ts
@@ -0,0 +1,58 @@
+﻿import {Component, Input, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {HttpModule}    from '@angular/http';
+import {WidgetComponent, WidgetService} from '../../../core/widgets';
+import {RESTService} from '../../../core/services';
+import {AppNavigator} from '../../../navigation/app.navigator.component';
+
+import * as wjFlexGrid from 'wijmo/wijmo.angular2.grid';
+import * as wjFlexGridFilter from 'wijmo/wijmo.angular2.grid.filter';
+import * as wjFlexGridGroup from 'wijmo/wijmo.angular2.grid.grouppanel';
+import * as wjFlexInput from 'wijmo/wijmo.angular2.input';
+
+
+declare var injectSVG: any;
+declare var bootstrapNavigator: any;
+
+@Component({
+    templateUrl: '/app/configurator/components/ibmDomino/Ibm-overall-sametime-statistics.component.html',
+    providers: [
+        HttpModule,
+        RESTService
+    ]
+})
+export class OverallSametimeStat implements OnInit {
+    @Input() settings: any;
+    deviceId: any;
+    data: wijmo.collections.CollectionView;
+    errorMessage: string;
+
+    constructor(private service: RESTService, private route: ActivatedRoute) { }
+
+    get pageSize(): number {
+        return this.data.pageSize;
+    }
+    set pageSize(value: number) {
+        if (this.data.pageSize != value) {
+            this.data.pageSize = value;
+            this.data.refresh();
+        }
+    }
+    ngOnInit() {
+
+        //this.route.params.subscribe(params => {
+        //    this.deviceId = params['service'];
+        //});
+        //this.service.get('/configurator/overall_domino_stat')
+        //    .subscribe(
+        //    (response) => {
+        //        this.data = new wijmo.collections.CollectionView(new wijmo.collections.ObservableArray(response.data));
+        //        this.data.pageSize = 10;
+        //    },
+        //    (error) => this.errorMessage = <any>error
+        //    );
+    }
+}
+
+
+
