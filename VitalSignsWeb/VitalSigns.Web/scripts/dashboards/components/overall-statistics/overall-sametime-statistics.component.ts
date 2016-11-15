86c1686b49ad3afc1cf136982327fa28923eb620
sowmya <sowmya@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-11-15 12:31:21 +0000

    overall sametime
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@1380 6b605366-3ce6-ac4f-954a-f758cfb76b34

---
+++ VitalSignsWeb/VitalSigns.Web/scripts/dashboards/components/overall-statistics/overall-sametime-statistics.component.ts
@@ -0,0 +1,54 @@
+﻿import {Component, Input, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {HttpModule}    from '@angular/http';
+import {WidgetComponent, WidgetService} from '../../../core/widgets';
+import {RESTService} from '../../../core/services';
+import {AppNavigator} from '../../../navigation/app.navigator.component';
+
+import * as wjFlexGrid from 'wijmo/wijmo.angular2.grid';
+import * as wjFlexGridFilter from 'wijmo/wijmo.angular2.grid.filter';
+import * as wjFlexGridGroup from 'wijmo/wijmo.angular2.grid.grouppanel';
+import * as wjFlexInput from 'wijmo/wijmo.angular2.input';
+
+
+declare var injectSVG: any;
+declare var bootstrapNavigator: any;
+
+@Component({
+    templateUrl: '/app/dashboards/components/overall-statistics/overall-sametime-statistics.component.html',
+    providers: [
+        HttpModule,
+        RESTService
+    ]
+})
+export class SametimeStatistics implements OnInit {
+    @Input() settings: any;
+    deviceId: any;
+    data: wijmo.collections.CollectionView;
+    errorMessage: string;
+
+    constructor(private service: RESTService, private route: ActivatedRoute) { }
+
+    get pageSize(): number {
+        return this.data.pageSize;
+    }
+    set pageSize(value: number) {
+        if (this.data.pageSize != value) {
+            this.data.pageSize = value;
+            this.data.refresh();
+        }
+    }
+    ngOnInit() {
+        this.service.get('/DashBoard/get_sametime_statistics')
+            .subscribe(
+            (response) => {
+                this.data = new wijmo.collections.CollectionView(new wijmo.collections.ObservableArray(response.data));
+                this.data.pageSize = 10;
+            },
+            (error) => this.errorMessage = <any>error
+            );
+    }
+}
+
+
+
