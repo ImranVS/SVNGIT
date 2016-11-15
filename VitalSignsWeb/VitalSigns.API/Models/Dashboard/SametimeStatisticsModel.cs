e6cbb03f86c87226ed98dfbda6d8450e175d1fbc
sowmya <sowmya@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-11-15 12:35:33 +0000

    overall sametime
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@1382 6b605366-3ce6-ac4f-954a-f758cfb76b34

---
+++ VitalSignsWeb/VitalSigns.API/Models/Dashboard/SametimeStatisticsModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace VitalSigns.API.Models.Dashboard
+{
+    public class SametimeStatisticsModel
+    {
+        [JsonProperty("device_name")]
+        public string DeviceName { get; set; }
+
+        [JsonProperty("totaln_way_chats")]
+        public int? TotalnWayChats { get; set; }
+
+        [JsonProperty("total2_way_chats")]
+        public int? Total2WayChats { get; set; }
+
+        [JsonProperty("peak_logins")]
+        public int? PeakLogins { get; set; }
+
+    }
+}
