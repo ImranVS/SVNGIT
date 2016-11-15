a85858fd808b0d1e483d675b2fd78939feb7a090
kiran <kiran@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-11-15 12:18:46 +0000

    FilterCount Issue fixed
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@1378 6b605366-3ce6-ac4f-954a-f758cfb76b34

--- VitalSignsWeb/VitalSigns.Web/scripts/services/components/services-view.component.ts
+++ VitalSignsWeb/VitalSigns.Web/scripts/services/components/services-view.component.ts
@@ -16,15 +16,55 @@ declare var injectSVG: any;
     ]
 })
 export class ServicesView implements OnInit, AfterViewChecked {
-    @Input() searchText
-    @Input() searchType;
-    @Input() searchStatus;
-    @Input() searchLocation;
+
+
+    filterCount = 0;
+    _searchText: string;
+    @Input('searchText')
+    public set searchText(val: string) {
+        this._searchText = val;
+        this.filterCount = 0;
+    }
+    public get searchText():string {
+        return this._searchText ;
+    }
+    
+    _searchType: string;
+    @Input('searchType')
+    public set searchType(val: string) {
+        this._searchType = val;
+        this.filterCount = 0;
+    }
+    public get searchType(): string {
+        return this._searchType;
+    }
+
+
+    _searchStatus: string;
+    @Input('searchType')
+    public set searchStatus(val: string) {
+        this._searchStatus = val;
+        this.filterCount = 0;
+    }
+    public get searchStatus(): string {
+        return this._searchStatus;
+    }
+      
+    _searchLocation: string;
+    @Input('searchType')
+    public set searchLocation(val: string) {
+        this._searchLocation = val;
+        this.filterCount = 0;
+    }
+    public get searchLocation(): string {
+        return this._searchLocation;
+    } 
 
     errorMessage: string;
     module: string;
     status: string;
     services: any[];
+   
 
     constructor(private service: RESTService, private router: Router, private route: ActivatedRoute) { }
 
@@ -34,13 +74,26 @@ export class ServicesView implements OnInit, AfterViewChecked {
         this.services.forEach(service => service.active = false);
         service.active = true;  
         
-        this.router.navigate(['services/' + this.module,  service.device_id ]);
+        this.router.navigate(['services/' + this.module,  service.id ]);
+    }
+    setFilterCount(index: any) {
+        this.filterCount= index+1;
     }
 
     loadData() {
         this.service.get('/Services/device_list')
             .subscribe(
-            data => this.services = data.data,
+            response => {
+                if (this.module == "dashboard") {                  
+                  var value=  response.data.filter((item) => item.is_enabled == true);
+                  this.services = value;
+                  console.log(value);
+                }
+                else {
+                    this.services = response.data;
+                }
+
+            },
             error => this.errorMessage = <any>error
             );
     }
