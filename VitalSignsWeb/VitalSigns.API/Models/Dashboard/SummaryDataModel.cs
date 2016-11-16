9213e7b3c372cc0639cb7a6db7c28599ac3f7698
sowmya <sowmya@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-11-16 08:42:40 +0000

    overall domino stat
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@1402 6b605366-3ce6-ac4f-954a-f758cfb76b34

---
+++ VitalSignsWeb/VitalSigns.API/Models/Dashboard/SummaryDataModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace VitalSigns.API.Models
+{
+    public class SummaryDataModel
+    {
+        [JsonProperty("device_id")]
+        public string DeviceID { get; set; }
+
+        [JsonProperty("stat_name")]
+        public string StatName { get; set; }
+
+        [JsonProperty("value")]
+        public double? Value { get; set; }
+    }
+}
