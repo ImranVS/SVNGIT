52a8c51f981dd63eb1d14c82f849c5734c74bfb7
sowjanya <sowjanya@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-11-16 14:40:18 +0000

    Dashboard: Tabs for URLs
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@1410 6b605366-3ce6-ac4f-954a-f758cfb76b34

---
+++ VitalSignsWeb/VitalSigns.Web/scripts/services/components/service-url-health.component.ts
@@ -0,0 +1,124 @@
+﻿import {Component, ComponentFactoryResolver, OnInit, Injector} from '@angular/core';
+
+import {WidgetController, WidgetContainer, WidgetContract, WidgetService} from '../../core/widgets';
+
+import {ServiceTab} from '../models/service-tab.interface';
+
+declare var injectSVG: any;
+
+@Component({
+    selector: 'tab-overall',
+    templateUrl: '/app/services/components/service-url-health.component.html',
+    providers: [WidgetService]
+})
+export class URLHealthTab extends WidgetController implements OnInit, ServiceTab {
+
+    widgets: WidgetContract[];
+    serviceId: string;
+    deviceId: any;
+
+    constructor(protected resolver: ComponentFactoryResolver, protected widgetService: WidgetService) {
+        super(resolver, widgetService);
+    }
+    
+    ngOnInit() {
+    
+        this.widgets = [
+            {
+                id: 'responseTime',
+                title: 'Response Time',
+                name: 'ChartComponent',
+                css: 'col-xs-12 col-sm-12 col-md-6 col-lg-6',
+                settings: {
+                    url: `/services/statistics?statname= ResponseTime&deviceId=${this.serviceId}&operation=hourly`,
+                    chart: {
+                        chart: {
+                            renderTo: 'responseTime',
+                            type: 'areaspline',
+                            height: 300
+                        },
+                        colors: ['#5fbe7f'],
+                        title: { text: '' },
+                        subtitle: { text: '' },
+                        xAxis: {
+                            labels: {
+                                step: 6
+                            },
+                            categories: [],
+                            title: {
+                                text: 'Time'
+                            }
+                        },
+                        yAxis: {
+                            title: {
+                                text: 'ms'
+                            }
+                        },
+                        legend: {
+                            enabled: false
+                        },
+                        credits: {
+                            enabled: false
+                        },
+                        exporting: {
+                            enabled: false
+                        },
+                        series: []
+                    }
+                }
+            },
+            {
+                id: 'hourlyupPercent',
+                title: 'Hourly Up Percent',
+                name: 'ChartComponent',
+                css: 'col-xs-12 col-sm-12 col-md-6 col-lg-6',
+                settings: {
+                    url: `/services/statistics?statname= HourlyUpTimePercent&deviceId=${this.serviceId}&operation=hourly`,
+                   
+                    chart: {
+                        chart: {
+                            renderTo: 'hourlyupPercent',
+                            type: 'areaspline',
+                            height: 300
+                        },
+                        colors: ['#5fbe7f'],
+                        title: { text: '' },
+                        subtitle: { text: '' },
+                        xAxis: {
+                            labels: {
+                                step: 6
+                            },
+                            categories: [],
+                            title: {
+                                text: 'Time'
+                            }
+                        },
+                        yAxis: {
+                            title: {
+                                text: 'Percent'
+                            }
+                        },
+                        legend: {
+                            enabled: false
+                        },
+                        credits: {
+                            enabled: false
+                        },
+                        exporting: {
+                            enabled: false
+                        },
+                        series: [{
+                            name: '% Usage',
+                            data: []
+                        }]
+                    }
+                }
+            }
+           
+            
+        ];
+    
+        injectSVG();
+    }
+
+}
