85ed4fe27d501e690a57bb9253dd0ec1ca17b5e0
sowmya <sowmya@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-11-17 12:43:10 +0000

    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@1436 6b605366-3ce6-ac4f-954a-f758cfb76b34

--- VitalSignsWeb/VitalSigns.API/Models/Dashboard/SametimeStatisticsModel.cs
+++ VitalSignsWeb/VitalSigns.API/Models/Dashboard/SametimeStatisticsModel.cs
@@ -4,21 +4,26 @@ using System.Linq;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 
-namespace VitalSigns.API.Models.Dashboard
+namespace VitalSigns.API.Models
 {
     public class SametimeStatisticsModel
     {
         [JsonProperty("device_name")]
         public string DeviceName { get; set; }
 
-        [JsonProperty("totaln_way_chats")]
-        public int? TotalnWayChats { get; set; }
+        //[JsonProperty("totaln_way_chats")]
+        //public int? TotalnWayChats { get; set; }
 
-        [JsonProperty("total2_way_chats")]
-        public int? Total2WayChats { get; set; }
+        //[JsonProperty("total2_way_chats")]
+        //public int? Total2WayChats { get; set; }
 
-        [JsonProperty("peak_logins")]
-        public int? PeakLogins { get; set; }
+        //[JsonProperty("peak_logins")]
+        //public int? PeakLogins { get; set; }
 
+        [JsonProperty("stat_name")]
+        public string StatName { get; set; }
+
+        [JsonProperty ("stat_value")]
+        public double? StatValue { get; set; }
     }
 }
