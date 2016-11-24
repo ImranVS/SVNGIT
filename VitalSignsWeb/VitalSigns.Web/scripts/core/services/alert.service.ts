93b4729565b1e4472c52bf6f94c8e8464e5684fc
sowmya <sowmya@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-11-24 13:13:15 +0000

    alert message
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@1570 6b605366-3ce6-ac4f-954a-f758cfb76b34

---
+++ VitalSignsWeb/VitalSigns.Web/scripts/core/services/alert.service.ts
@@ -0,0 +1,20 @@
+﻿import { Injectable } from '@angular/core';
+
+import { AppComponent } from '../../app.component';
+
+@Injectable()
+export class AlertService {
+   
+    private _appComponent: AppComponent;
+
+    registerAppComponentView(appComponent: AppComponent) {
+        console.log(appComponent);
+        this._appComponent = appComponent;
+    }
+
+    showAlertMessage(_type: string, _message: string, _dismissOnTimeout: number = 5000) {
+        console.log(this._appComponent);
+        this._appComponent.showAlert(_type, _message, _dismissOnTimeout);
+    }
+
+}
