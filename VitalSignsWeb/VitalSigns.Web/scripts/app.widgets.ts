97df22cd2080c221ce44a80e5791ce914ad05dcb
sowmya <sowmya@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-11-24 06:46:14 +0000

    server event configurator
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@1559 6b605366-3ce6-ac4f-954a-f758cfb76b34

--- VitalSignsWeb/VitalSigns.Web/scripts/app.widgets.ts
+++ VitalSignsWeb/VitalSigns.Web/scripts/app.widgets.ts
@@ -67,6 +67,7 @@ export * from './configurator/components/server/server-simulation-tests.compomen
 export * from './configurator/components/server/server-device-attributes.component'
 
 export * from './configurator/components/server/server-maintenance-windows.component'
+export * from './configurator/components/server/server-events.component'
 
 export * from './configurator/components/server/server-disk-settings.component'
 export * from './configurator/components/server/server-advanced-settings.component'
