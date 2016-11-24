74b0c3bb6592b90bcdbe5fb27f899a5d13372eb9
sowmya <sowmya@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-11-24 13:13:50 +0000

    alert message
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@1571 6b605366-3ce6-ac4f-954a-f758cfb76b34

--- VitalSignsWeb/VitalSigns.Web/scripts/app.component.ts
+++ VitalSignsWeb/VitalSigns.Web/scripts/app.component.ts
@@ -2,6 +2,7 @@
 
 import {AppHeader} from './navigation/app.header.component';
 import {AppMainMenu} from './navigation/app.main-menu.component';
+import {AlertService} from './core/services/alert.service';
 
 declare var bootstrapZeus: any;
 declare var injectSVG: any;
@@ -14,15 +15,59 @@ declare var injectSVG: any;
         <app-main-menu></app-main-menu>
     </div>
     <div id="zeusHeader">
+
         <app-header></app-header>
     </div>
     <div id="zeusWrapper">
         <router-outlet></router-outlet>
     </div>
+    <div class="topCorner">
+        <div class="alert" role="alert" [class]="classes" *ngIf="!closed">
+            <button *ngIf="closeable" type="button" class="close" (click)="onClose($event)">
+                <span aria-hidden="true">&times;</span>
+                <span class="sr-only">Close</span>
+            </button>
+            <span> {{message}}</span>
+        </div>
+    </div>
 </div>
-`
+`,
+    providers: [
+        AlertService
+    ]
 })
+
 export class AppComponent implements OnInit {
+     dismissOnTimeout: number;
+     type: string;
+     message: string;
+     private closed: boolean = true;
+     private closeable: boolean = true;
+     private classes: string;  
+     
+     constructor(private alertService: AlertService) {
+
+         this.alertService.registerAppComponentView(this);
+     }
+     showAlert(_type: string, _message: string, _dismissOnTimeout: number) {
+         this.closed = false;
+         this.type = _type || 'warning';
+         this.message = _message;
+         this.classes ="alert "
+         this.classes = this.classes + ' alert-' + this.type;
+         if (this.closeable) {
+             this.classes = this.classes + ' alert-dismissible';
+         }
+         this.dismissOnTimeout = _dismissOnTimeout ||0;
+         if (this.dismissOnTimeout > 0) {
+             let close = this.onClose.bind(this);
+             setTimeout(close, this.dismissOnTimeout);
+         }
+     }
+
+     onClose(event: MouseEvent) {        
+         this.closed = true;
+     }
     ngOnInit() {
         bootstrapZeus();
         injectSVG();
