1bbff868f32108f8b6ba6690db1f50a008541e6f
sowmya <sowmya@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-12-01 09:43:55 +0000

    traveler datastore success message
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@1655 6b605366-3ce6-ac4f-954a-f758cfb76b34

--- VitalSignsWeb/VitalSigns.Web/scripts/configurator/components/ibmDomino/Ibm-travelerdatastore.component.ts
+++ VitalSignsWeb/VitalSigns.Web/scripts/configurator/components/ibmDomino/Ibm-travelerdatastore.component.ts
@@ -1,15 +1,10 @@
 ﻿import {Component, OnInit,ViewChild} from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {CommonModule} from '@angular/common';
 import {HttpModule}    from '@angular/http';
 import {RESTService} from '../../../core/services';
 import {GridBase} from '../../../core/gridBase';
-
 import {AppNavigator} from '../../../navigation/app.navigator.component';
-import * as wjFlexGrid from 'wijmo/wijmo.angular2.grid';
-import * as wjFlexGridFilter from 'wijmo/wijmo.angular2.grid.filter';
-import * as wjFlexGridGroup from 'wijmo/wijmo.angular2.grid.grouppanel';
-import * as wjFlexInput from 'wijmo/wijmo.angular2.input';
-import * as wjCoreModule from 'wijmo/wijmo.angular2.core';;
+import {AppComponentService} from '../../../core/services';
 
 @Component({
     templateUrl: '/app/configurator/components/ibmDomino/Ibm-travelerdatastore.component.html',
@@ -25,8 +20,8 @@ export class TravelerDataStore extends GridBase implements OnInit {
     usersByserver: any = [];
     checkedItems: any[];
 
-    constructor(service: RESTService) {
-        super(service);
+    constructor(service: RESTService, appComponentService: AppComponentService) {
+        super(service, appComponentService);
         this.formName = "Traveler Data Store";
     }              
     ngOnInit() {       
@@ -66,7 +61,6 @@ export class TravelerDataStore extends GridBase implements OnInit {
         }     
     }
     addTravelerData(dlg: wijmo.input.Popup, servers: wijmo.input.MultiSelect) {
-        console.log(servers);
         this.addGridRow(dlg);
         this.currentEditItem.traveler_service_pool_name = "";
         this.currentEditItem.device_name = "";
@@ -80,5 +74,4 @@ export class TravelerDataStore extends GridBase implements OnInit {
         this.currentEditItem.used_by_servers = [];
         this.checkedItems = [];
     }
-
 }
