91edf8afa5aa8b5a4056f620bbe3c8e468417d5a
sowmya <sowmya@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-12-09 13:48:26 +0000

    Maintain Users
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@1800 6b605366-3ce6-ac4f-954a-f758cfb76b34

--- VitalSignsWeb/VitalSigns.Web/scripts/configurator/components/applicationSettings/application-settings-maintainusers.component.ts
+++ VitalSignsWeb/VitalSigns.Web/scripts/configurator/components/applicationSettings/application-settings-maintainusers.component.ts
@@ -14,33 +14,76 @@ import {AppComponentService} from '../../../core/services';
     ]
 })
 export class MaintainUser extends GridBase implements OnInit {
+    errorMessage: any;
     status: any;
-    superadmin: any;
+    maintainRoles: any;
+    usersRoles: any = [];
+    checkedItems: any[];
+
     constructor(service: RESTService, appComponentService:AppComponentService) {
         super(service, appComponentService);
-        this.formName = "User";
-        this.status = ["Active", "Inactive"];
-        this.superadmin = ["Yes", "No"];
-
+        this.formName = "User";     
     }
     ngOnInit() {
+        //this.service.get('/configurator/get_maintain_users')
+        //    .subscribe(
+        //    (response) => {
+        //        this.maintainRoles = new wijmo.collections.CollectionView(new wijmo.collections.ObservableArray(response.data));
+        //        console.log(response.data);
+        //    },
+        //    (error) => this.errorMessage = <any>error
+        //    );
         this.initialGridBind('/configurator/get_maintain_users');
+        this.maintainRoles = new wijmo.collections.CollectionView(new wijmo.collections.ObservableArray(["Configurator","UserManager","RemoteConsole"]));
     }
-    saveMaintainUser(dlg: wijmo.input.Popup) {      
+    rolesChecked(userrole: wijmo.input.MultiSelect) {
+        this.usersRoles = [];
+        for (var item of userrole.checkedItems) {
+            this.usersRoles.push(item);
+        }
+    }
+    resetPassWord() {
+    
+
+        this.service.get(`/configurator/reset_password/?emailId=${this.flex.collectionView.currentItem.email}`)
+            .subscribe(
+            response => {
+
+
+            },
+            error => this.errorMessage = <any>error
+            );
+    }
+    saveMaintainUser(dlg: wijmo.input.Popup) {
+        this.currentEditItem.roles = this.usersRoles;  
         this.saveGridRow('/configurator/save_maintain_users', dlg);
     }
     delteMaintainUsers() {
         this.delteGridRow('/configurator/delete_maintain_users/');
     }
-    addMaintainUser(dlg: wijmo.input.Popup) {
+    editUserGridRow(dlg: wijmo.input.Popup) {
+        this.editGridRow(dlg);
+        this.checkedItems = [];
+        var roles = this.currentEditItem.roles;
+        console.log(roles);
+        if (roles) {
+            for (var userItem of (<wijmo.collections.CollectionView>this.maintainRoles).sourceCollection) {
+                console.log(userItem);
+                var urole = roles.filter((item) => item == userItem);
+
+                console.log("role: "+urole);
+                if (urole.length > 0)
+                    this.checkedItems.push(userItem);
+            }
+        }
+    }
+    addMaintainUser(dlg: wijmo.input.Popup, userrole: wijmo.input.MultiSelect) {
         this.addGridRow(dlg);
-        this.currentEditItem.login_name = "";
-        this.currentEditItem.full_name = "";
         this.currentEditItem.email = "";
-        this.currentEditItem.status = "Active";
-        this.currentEditItem.super_admin = "No";
-        this.currentEditItem.configurator_access = false;
-        this.currentEditItem.console_command_access = false;
+        this.currentEditItem.full_name = "";
+        this.currentEditItem.roles = [];
+        this.currentEditItem.status = true;
+        this.checkedItems = [];
     }
 }
 
