196f8286324cfffcf47793d0a107831c213799ec
sowjanya <sowjanya@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-12-13 11:56:05 +0000

    VSPLUS-3396 Dashboard: Maintenance tab is Not Implemented on the server details page 3 of 6
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@1845 6b605366-3ce6-ac4f-954a-f758cfb76b34

--- VitalSignsWeb/VitalSigns.Web/scripts/configurator/components/server/server-maintenance-windows.component.ts
+++ VitalSignsWeb/VitalSigns.Web/scripts/configurator/components/server/server-maintenance-windows.component.ts
@@ -20,6 +20,20 @@ import * as helpers from '../../../core/services/helpers/helpers';
 //export class ServerDiskSettings implements OnInit, AfterViewInit {
 export class MaintenanceWindows implements OnInit {
     deviceId: any;
+    from_date: any;
+    to_date: any;
+    from_time: any;
+    to_time: any;
+    fhour: string;
+    thour: string;
+    fhourInt: any;
+    thourInt: any;
+    fminute: any;
+    tminute: any;
+    fminuteInt: any;
+    tminuteInt: any;
+    maintenanceForm: FormGroup;
+    module: any;
     data: wijmo.collections.CollectionView;
     errorMessage: string;
      @ViewChild('flex') flex: wijmo.grid.FlexGrid;
@@ -30,9 +44,17 @@ export class MaintenanceWindows implements OnInit {
 
         this.route.params.subscribe(params => {
             this.deviceId = params['service'];
+           
 
         });
-        this.dataProvider.get('/Configurator/get_server_maintenancedata/' + this.deviceId +'')
+
+        this.maintenanceForm = this.formBuilder.group({
+            'from_date': [''],
+           
+
+        });
+       
+        this.dataProvider.get('/Configurator/get_server_maintenancedata/' + this.deviceId + '')
             .subscribe(
             response => {
                 this.data = new wijmo.collections.CollectionView(new wijmo.collections.ObservableArray(this.datetimeHelpers.toLocalDateTime(response.data)));
@@ -43,7 +65,13 @@ export class MaintenanceWindows implements OnInit {
        
     }
 
- ngOnInit() {
+    ngOnInit() {
+        this.route.parent.params.subscribe(params => {
+
+            this.module = params['module'];
+            console.log(this.module);
+
+        });
        
     }
 
@@ -56,6 +84,102 @@ export class MaintenanceWindows implements OnInit {
             this.data.pageSize = value;
             this.data.refresh();
         }
+   }
+
+    clearClick() {
+        console.log("sowji");
+        console.log(this.from_date);
+        this.from_date = "";
+        this.to_date = "";
+        this.from_time = "";
+        this.to_time = "";
+        this.fillMaintenance();
+        
+       
     }
+
+    searchClick() {
+
+        this.fillMaintenance();
+
+    }
+
+    fillMaintenance() {
+        
+        if (this.from_date != "" && this.to_date != "") {
+            if (this.from_date > this.to_date) {
+                alert("From Date value should be less than To Date.");
+            }
+            else {
+
+                if ((this.from_date != null && this.to_date != null) && (this.from_date != "" && this.from_date != "")) {
+                    this.fhour = this.from_time.Substring(0, this.from_time.IndexOf(":"));
+                    this.thour = this.to_time.Substring(0, this.to_time.IndexOf(":"));
+                    this.fhourInt = (this.fhour);
+                    this.thourInt = (this.thour);
+                    this.fminute = this.from_time.Substring(3, 2);
+                    this.tminute = this.to_time.Substring(3, 2);
+                    this.fminuteInt = (this.fminute);
+                    this.tminuteInt = (this.tminute);
+                    if (this.fhourInt >= 24 || this.thourInt >= 24 || this.fminuteInt >= 60 || this.tminuteInt >= 60) {
+
+                        alert("Invalid hour/minute entry.");
+                    }
+
+                    else {
+                        this.dataProvider.get('/Configurator/get_server_maintenancedata/' + this.deviceId + '' + this.from_date + '' + this.to_date + '' + this.from_time + '' + this.to_time + '')
+                            .subscribe(
+                            response => {
+                                this.data = new wijmo.collections.CollectionView(new wijmo.collections.ObservableArray(this.datetimeHelpers.toLocalDateTime(response.data)));
+                                this.data.pageSize = 10;
+
+                            });
+                    }
+                }
+            }
+        }
+
+        else {
+
+            if ((this.from_time != null && this.to_time != null) && (this.from_time != "" && this.to_time != "")) {
+                this.fhour = this.from_time.Substring(0, this.from_time.IndexOf(":"));
+                this.thour = this.to_time.Substring(0, this.to_time.IndexOf(":"));
+                this.fhourInt = (this.fhour);
+                this.thourInt = (this.thour);
+                this.fminute = this.from_time.Substring(3, 2);
+                this.tminute = this.to_time.Substring(3, 2);
+                this.fminuteInt = (this.fminute);
+                this.tminuteInt = (this.tminute);
+                if (this.fhourInt >= 24 || this.thourInt >= 24 || this.fminuteInt >= 60 || this.tminuteInt >= 60) {
+
+                    alert("Invalid hour/minute entry.");
+                }
+
+                else {
+                    this.dataProvider.get('/Configurator/get_server_maintenancedata/' + this.deviceId + '' + this.from_date + '' + this.to_date + '' + this.from_time + '' + this.to_time + '')
+                        .subscribe(
+                        response => {
+                            this.data = new wijmo.collections.CollectionView(new wijmo.collections.ObservableArray(this.datetimeHelpers.toLocalDateTime(response.data)));
+                            this.data.pageSize = 10;
+
+                        });
+                }
+            }
+         
+            else {
+                this.dataProvider.get('/Configurator/get_server_maintenancedata/' + this.deviceId + '' + this.from_date + '' + this.to_date + '' + this.from_time + '' + this.to_time + '')
+                    .subscribe(
+                    response => {
+                        this.data = new wijmo.collections.CollectionView(new wijmo.collections.ObservableArray(this.datetimeHelpers.toLocalDateTime(response.data)));
+                        this.data.pageSize = 10;
+
+                    });
+            }
+        }
+
+
+       
+    }
+
    
 }
