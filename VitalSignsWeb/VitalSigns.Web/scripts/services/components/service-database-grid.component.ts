9e89459a9ef7f01e2778283191a68b341f1ec360
sowmya <sowmya@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-12-15 10:09:21 +0000

    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@1905 6b605366-3ce6-ac4f-954a-f758cfb76b34

--- VitalSignsWeb/VitalSigns.Web/scripts/services/components/service-database-grid.component.ts
+++ VitalSignsWeb/VitalSigns.Web/scripts/services/components/service-database-grid.component.ts
@@ -73,9 +73,6 @@ export class ServiceDatabaseGrid implements OnInit {
     }
 
     refreshChart(event: wijmo.grid.CellRangeEventArgs) {
-
-        console.log(event.panel.grid.selectedItems);
-
         this.widgetService.refreshWidget('responseTimes')
             .catch(error => console.log(error));
 
