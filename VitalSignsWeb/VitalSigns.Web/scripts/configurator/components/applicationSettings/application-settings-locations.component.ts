43904a2ee16ceb2beb6341d3428ed7404ecd20c2
swathid <swathid@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-12-15 09:12:25 +0000

    Vsplus-3360
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@1886 6b605366-3ce6-ac4f-954a-f758cfb76b34

--- VitalSignsWeb/VitalSigns.Web/scripts/configurator/components/applicationSettings/application-settings-locations.component.ts
+++ VitalSignsWeb/VitalSigns.Web/scripts/configurator/components/applicationSettings/application-settings-locations.component.ts
@@ -32,113 +32,56 @@ export class Location extends GridBase implements OnInit  {
     countries: any;
     states: any;
     cities: any;
-    state: any;
-    city: any;
-    mode: string;
-    setcountry: string;
-    setregion: string;
-    setcity: string;
+
     constructor(service: RESTService, appComponentService: AppComponentService) {
         super(service, appComponentService);
+
         this.formName = "Location";
+
 
         this.service.get('/Configurator/get_locations')
             .subscribe(
             (response) => {
                 this.countries = response.data.countryData;
-
             },
             (error) => this.errorMessage = <any>error
-            );
+        );
 
       
     }
 
 
     savelocations(dlg: wijmo.input.Popup) {
-        this.saveGridRow('/Configurator/save_locations', dlg);
-      
+        this.saveGridRow('/Configurator/save_locations',dlg);
     }
 
 
     ngOnInit() {
         this.initialGridBind('/Configurator/locations');
-       
         }
-    editLocationsRow(dlg: wijmo.input.Popup) {
+ 
 
-        this.editGridRow(dlg);
-        this.mode = "edit"; 
-        this.setcountry = this.currentEditItem.country;
-        this.setregion = this.currentEditItem.region;
-        this.setcity = this.currentEditItem.city; 
-    }
-
-    getstates() {
-        
-        this.service.get('/Configurator/get_locations?country=' + this.currentEditItem.country)
-
-            .subscribe(
-            (response) => {             
-                this.states = response.data.stateData; 
-                this.currentEditItem.country = this.setcountry; 
-                this.currentEditItem.region = this.setregion;
-                this.currentEditItem.city = this.setcity;             
-                
-            },
-            (error) => this.errorMessage = <any>error
-        );
-       
-    }
-    getstate() {
-
-        this.service.get('/Configurator/get_locations?country=' + this.currentEditItem.country)
+    getstates(currentregion) {
+      
+        this.service.get('/Configurator/get_locations?country=' + currentregion)
 
             .subscribe(
             (response) => {
-                this.state = response.data.stateData;
-               // this.currentEditItem.country = this.setcountry;
-               this.currentEditItem.region = this.setregion;
-                this.currentEditItem.city = this.setcity;
-
+                this.states = response.data.stateData;
+               // this.states.splice(0, 1);
             },
             (error) => this.errorMessage = <any>error
             );
-
     }
-
-  
-    getcities() {
+    getcities(currentitem) {
        
-        this.service.get('/Configurator/get_locations?country=' + this.currentEditItem.country + '&state=' + this.currentEditItem.region)
+        this.service.get('/Configurator/get_locations?country=' + this.currentEditItem.country + '&state=' + currentitem)
             .subscribe(
-            (response) => {   
-               //  this.currentEditItem.city = this.currentEditItem.city;
-                this.cities = response.data.cityData;
-                this.currentEditItem.country = this.setcountry;
-                this.currentEditItem.region = this.setregion;
-                this.currentEditItem.city = this.setcity;  
-               // this.currentEditItem.city = this.currentEditItem.city;
-            },
-            (error) => this.errorMessage = <any>error
-        );
-        
-    }
-    getcity() {
-
-        this.service.get('/Configurator/get_locations?country=' + this.currentEditItem.country + '&state=' + this.currentEditItem.region)
-            .subscribe(
-            (response) => {
-                //  this.currentEditItem.city = this.currentEditItem.city;
-                this.city = response.data.cityData;
-                this.currentEditItem.country = this.setcountry;
-             // this.currentEditItem.region = this.setregion;
-                this.currentEditItem.city = this.setcity;
-                // this.currentEditItem.city = this.currentEditItem.city;
+            (response) => {               
+                this.cities = response.data.cityData 
             },
             (error) => this.errorMessage = <any>error
             );
-
     }
    
     deltelocations() {
@@ -149,12 +92,9 @@ export class Location extends GridBase implements OnInit  {
 
     addlocations(dlg: wijmo.input.Popup) {
         this.addGridRow(dlg);
-        this.mode = "add";
         this.currentEditItem.location_name = "";
         this.currentEditItem.country = "";
         this.currentEditItem.region = "";
         this.currentEditItem.city = "";
-        console.log(this.mode);
-        console.log(this.currentEditItem.country);
     }
 }
