6ccb0db72d6e134f0b8adf7a0407b04f52b1f936
sowjanya <sowjanya@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-12-15 10:46:30 +0000

    Configurator: Notes Databases - based on the drop down selection Type, the field below should change accordingly
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@1927 6b605366-3ce6-ac4f-954a-f758cfb76b34

--- VitalSignsWeb/VitalSigns.Web/scripts/configurator/components/ibmDomino/Ibm-notes-databases.component.ts
+++ VitalSignsWeb/VitalSigns.Web/scripts/configurator/components/ibmDomino/Ibm-notes-databases.component.ts
@@ -31,7 +31,7 @@ export class NotesDatabases extends GridBase implements OnInit {
     addNotesDatabase(dlg: wijmo.input.Popup, servers: wijmo.input.MultiSelect) {
         this.addGridRow(dlg);
         this.currentEditItem.name = "";
-        this.currentEditItem.is_enabled = "";
+        this.currentEditItem.is_enabled = false;
         this.currentEditItem.domino_server_name = "";
         this.currentEditItem.database_file_name = "";
         this.currentEditItem.scan_interval = "8";
@@ -39,6 +39,7 @@ export class NotesDatabases extends GridBase implements OnInit {
         this.currentEditItem.off_hours_scan_interval = "30";
         this.currentEditItem.trigger_type = null;
         this.currentEditItem.trigger_value = null;
+        this.currentEditItem.initiate_replication = false;
         this.currentEditItem.replication_destination = null;
         this.checkedItems = null;
         this.usersByserver = null;
@@ -65,27 +66,27 @@ export class NotesDatabases extends GridBase implements OnInit {
         this.delteGridRow('/configurator/delete_notes_database/');
     }
 
-    serversChecked(servers: wijmo.input.MultiSelect) {
-        this.usersByserver = null;
-        for (var item of servers.checkedItems) {
-            this.usersByserver.push(item.value);
-        }
-    }
+    //serversChecked(servers: wijmo.input.MultiSelect) {
+    //    this.usersByserver = null;
+    //    for (var item of servers.checkedItems) {
+    //        this.usersByserver.push(item.value);
+    //    }
+    //}
 
-    editNotesDatabase(dlg: wijmo.input.Popup, servers: wijmo.input.MultiSelect) {
+    editNotesDatabase(dlg: wijmo.input.Popup) {
         this.editGridRow(dlg);
-        this.checkedItems = null;
-        this.usersByserver = null;
-        var replicationdestination = this.currentEditItem.replication_destination;
-        if (replicationdestination) {
-            if (servers)
-                 servers.refresh(true);
-            for (var travelerItem of (<wijmo.collections.CollectionView>this.dominoServers).sourceCollection) {
-                var server = replicationdestination.filter((item) => item == travelerItem.value);
-                if (server.length > 0)
-                    this.checkedItems.push(travelerItem);
-            }
-        }
+        //this.checkedItems = null;
+        //this.usersByserver = null;
+        //var replicationdestination = this.currentEditItem.replication_destination;
+        //if (replicationdestination) {
+        //    if (servers)
+        //         servers.refresh(true);
+        //    for (var travelerItem of (<wijmo.collections.CollectionView>this.dominoServers).sourceCollection) {
+        //        var server = replicationdestination.filter((item) => item == travelerItem.value);
+        //        if (server.length > 0)
+        //            this.checkedItems.push(travelerItem);
+        //    }
+        //}
     }
 }
 
