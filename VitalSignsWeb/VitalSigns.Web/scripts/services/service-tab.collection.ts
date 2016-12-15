297fef09cac6b4216cd4a44483733a382b7476db
swathid <swathid@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-12-15 05:23:27 +0000

    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@1884 6b605366-3ce6-ac4f-954a-f758cfb76b34

--- VitalSignsWeb/VitalSigns.Web/scripts/services/service-tab.collection.ts
+++ VitalSignsWeb/VitalSigns.Web/scripts/services/service-tab.collection.ts
@@ -41,6 +41,7 @@ export * from '../configurator/components/server/server-simulation-tests.compome
 export * from '../configurator/components/server/server-maintenance-windows.component'
 export * from '../configurator/components/server/server-disk-settings.component'
 export * from '../configurator/components/server/server-advanced-settings.component'
+export * from '../configurator/components/server/server-websphere-settings.component'
 
 export * from '../configurator/components/server/server-device-attributes.component'
 
