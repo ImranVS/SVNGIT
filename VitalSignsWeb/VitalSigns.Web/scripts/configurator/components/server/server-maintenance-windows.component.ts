2e6acb5926942fade38a907ebc79151ab7d82c6d
sowjanya <sowjanya@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-12-16 13:52:28 +0000

    VSPLUS-3396 Dashboard: Maintenance tab is Not Implemented on the server details page
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@1935 6b605366-3ce6-ac4f-954a-f758cfb76b34

--- VitalSignsWeb/VitalSigns.Web/scripts/configurator/components/server/server-maintenance-windows.component.ts
+++ VitalSignsWeb/VitalSigns.Web/scripts/configurator/components/server/server-maintenance-windows.component.ts
@@ -38,7 +38,7 @@ export class MaintenanceWindows implements OnInit {
     data: wijmo.collections.CollectionView;
     errorMessage: string;
      @ViewChild('flex') flex: wijmo.grid.FlexGrid;
-
+     @ViewChild('filter') filter: wijmo.grid.filter.FlexGridFilter;
     constructor(
         private dataProvider: RESTService,
         private formBuilder: FormBuilder, private route: ActivatedRoute, private datetimeHelpers: helpers.DateTimeHelper) {
@@ -85,106 +85,104 @@ export class MaintenanceWindows implements OnInit {
         }
    }
 
-    clearClick() {
-        this.from_date = "";
-        this.to_date = "";
-        this.from_time = "";
-        this.to_time = "";
-        this.fillMaintenance();
+    //clearClick() {
+    //    this.from_date = "";
+    //    this.to_date = "";
+    //    this.from_time = "";
+    //    this.to_time = "";
+    //    this.fillMaintenance();
         
        
-    }
+    //}
+    //dateChanged() {
+    //  // this.filter.defaultFilterType = wijmo.grid.filter.FilterType.Condition;
+    //    var col = this.flex.columns.getColumn("StartDate"),
+    //       cf = this.filter.getColumnFilter(col);
+    //    cf.filterType = wijmo.grid.filter.FilterType.Condition;
+    //    cf.conditionFilter.condition1.operator = wijmo.grid.filter.Operator.GE;
+    //    cf.conditionFilter.condition1.value = this.from_date;       
+    //    cf.apply(this.from_date);
+    //}
+    //searchClick() {
+    //    this.fillMaintenance();
+    //}
 
-    searchClick() {
+    //fillMaintenance() {
+       
+    //    if (this.from_time == "undefined" || this.to_time == "undefined") {
 
-        this.fillMaintenance();
+    //        this.from_date = "";
+    //        this.to_time = ""
+    //    }
 
-    }
-
-    fillMaintenance() {
-        //this.service.get('/Token/reset_password?emailId=' + this.authService.CurrentUser.email + '&password=' + passwordVal)
-        //this.dataProvider.get('/Configurator/get_server_maintenancedata?id=' + this.deviceId + '&fromDate=' + this.from_date + '&toDate=' + this.to_date + '&fromTime=' + this.from_time + '&toTime=' + this.to_time)
-        //    .subscribe(
-        //    response => {
-        //        this.data = new wijmo.collections.CollectionView(new wijmo.collections.ObservableArray(this.datetimeHelpers.toLocalDateTime(response.data)));
-        //        this.data.pageSize = 10;
-        //    });
-
-      
-        if (this.from_time == "undefined" || this.to_time == "undefined") {
-
-            this.from_date = "";
-            this.to_time = ""
-        }
-
-        if (this.from_date != "" && this.to_date != "") {
+    //    if (this.from_date != "" && this.to_date != "") {
           
-            if (this.from_date > this.to_date) {
-                alert("From Date value should be less than To Date.");
-            }
-            else {
+    //        if (this.from_date > this.to_date) {
+    //            alert("From Date value should be less than To Date.");
+    //        }
+    //        else {
               
-                if ((this.from_time != null && this.to_time != null) && (this.from_time != "" && this.to_time != "")) {                                  
-                    this.fhour = this.from_time.substring(0, this.from_time.indexOf(":"));
+    //            if ((this.from_time != null && this.to_time != null) && (this.from_time != "" && this.to_time != "")) {                                  
+    //                this.fhour = this.from_time.substring(0, this.from_time.indexOf(":"));
                    
-                    this.thour = this.to_time.substring(0, this.to_time.indexOf(":"));
-                    this.fhourInt = (this.fhour);
-                    this.thourInt = (this.thour);
-                    this.fminute = this.from_time.substring(3, 2);
-                    this.tminute = this.to_time.substring(3, 2);
-                    this.fminuteInt = (this.fminute);
-                    this.tminuteInt = (this.tminute);
+    //                this.thour = this.to_time.substring(0, this.to_time.indexOf(":"));
+    //                this.fhourInt = (this.fhour);
+    //                this.thourInt = (this.thour);
+    //                this.fminute = this.from_time.substring(3, 2);
+    //                this.tminute = this.to_time.substring(3, 2);
+    //                this.fminuteInt = (this.fminute);
+    //                this.tminuteInt = (this.tminute);
                 
-                    if (this.fhourInt >= 24 || this.thourInt >= 24 || this.fminuteInt >= 60 || this.tminuteInt >= 60) {
+    //                if (this.fhourInt >= 24 || this.thourInt >= 24 || this.fminuteInt >= 60 || this.tminuteInt >= 60) {
                       
-                        alert("Invalid hour/minute entry.");
-                    }
-                    else {
+    //                    alert("Invalid hour/minute entry.");
+    //                }
+    //                else {
                       
-                        this.dataProvider.get('/Configurator/get_server_maintenancedata?id=' + this.deviceId + '&fromDate=' + this.from_date + '&toDate=' + this.to_date + '&fromTime=' + this.from_time + '&toTime=' + this.to_time)
-                            .subscribe(
-                            response => {
-                                this.data = new wijmo.collections.CollectionView(new wijmo.collections.ObservableArray(this.datetimeHelpers.toLocalDateTime(response.data)));
-                                this.data.pageSize = 10;
-                            });
-                    }
-                }
-            }
-        }
-        else
-        {
+    //                    this.dataProvider.get('/Configurator/get_server_maintenancedata?id=' + this.deviceId + '&fromDate=' + this.from_date + '&toDate=' + this.to_date + '&fromTime=' + this.from_time + '&toTime=' + this.to_time)
+    //                        .subscribe(
+    //                        response => {
+    //                            this.data = new wijmo.collections.CollectionView(new wijmo.collections.ObservableArray((response.data)));
+    //                            this.data.pageSize = 10;
+    //                        });
+    //                }
+    //            }
+    //        }
+    //    }
+    //    else
+    //    {
          
-            if ((this.from_time != null && this.to_time != null) && (this.from_time != "" && this.to_time != "")) {
-                this.fhour = this.from_time.substring(0, this.from_time.indexOf(":"));
-                this.thour = this.to_time.substring(0, this.to_time.IndexOf(":"));
-                this.fhourInt = (this.fhour);
-                this.thourInt = (this.thour);
-                this.fminute = this.from_time.substring(3, 2);
-                this.tminute = this.to_time.substring(3, 2);
-                this.fminuteInt = (this.fminute);
-                this.tminuteInt = (this.tminute);
-                if (this.fhourInt >= 24 || this.thourInt >= 24 || this.fminuteInt >= 60 || this.tminuteInt >= 60) {
-                    alert("Invalid hour/minute entry.");
-                }
+    //        if ((this.from_time != null && this.to_time != null) && (this.from_time != "" && this.to_time != "")) {
+    //            this.fhour = this.from_time.substring(0, this.from_time.indexOf(":"));
+    //            this.thour = this.to_time.substring(0, this.to_time.IndexOf(":"));
+    //            this.fhourInt = (this.fhour);
+    //            this.thourInt = (this.thour);
+    //            this.fminute = this.from_time.substring(3, 2);
+    //            this.tminute = this.to_time.substring(3, 2);
+    //            this.fminuteInt = (this.fminute);
+    //            this.tminuteInt = (this.tminute);
+    //            if (this.fhourInt >= 24 || this.thourInt >= 24 || this.fminuteInt >= 60 || this.tminuteInt >= 60) {
+    //                alert("Invalid hour/minute entry.");
+    //            }
 
-                else {
-                    this.dataProvider.get('/Configurator/get_server_maintenancedata?id=' + this.deviceId + '&fromDate=' + this.from_date + '&toDate=' + this.to_date + '&fromTime=' + this.from_time + '&toTime=' + this.to_time)
-                        .subscribe(
-                        response => {
-                            this.data = new wijmo.collections.CollectionView(new wijmo.collections.ObservableArray(this.datetimeHelpers.toLocalDateTime(response.data)));
-                            this.data.pageSize = 10;
-                        });
-                }
-            }
+    //            else {
+    //                this.dataProvider.get('/Configurator/get_server_maintenancedata?id=' + this.deviceId + '&fromDate=' + this.from_date + '&toDate=' + this.to_date + '&fromTime=' + this.from_time + '&toTime=' + this.to_time)
+    //                    .subscribe(
+    //                    response => {
+    //                        this.data = new wijmo.collections.CollectionView(new wijmo.collections.ObservableArray((response.data)));
+    //                        this.data.pageSize = 10;
+    //                    });
+    //            }
+    //        }
          
-            else {
-                this.dataProvider.get('/Configurator/get_server_maintenancedata?id=' + this.deviceId + '&fromDate=' + this.from_date + '&toDate=' + this.to_date + '&fromTime=' + this.from_time + '&toTime=' + this.to_time)
-                    .subscribe(
-                    response => {
-                        this.data = new wijmo.collections.CollectionView(new wijmo.collections.ObservableArray(this.datetimeHelpers.toLocalDateTime(response.data)));
-                        this.data.pageSize = 10;
-                        });
-            }
-        }
-    }
+    //        else {
+    //            this.dataProvider.get('/Configurator/get_server_maintenancedata?id=' + this.deviceId + '&fromDate=' + this.from_date + '&toDate=' + this.to_date + '&fromTime=' + this.from_time + '&toTime=' + this.to_time)
+    //                .subscribe(
+    //                response => {
+    //                    this.data = new wijmo.collections.CollectionView(new wijmo.collections.ObservableArray((response.data)));
+    //                    this.data.pageSize = 10;
+    //                    });
+    //        }
+    //    }
+    //}
 }
