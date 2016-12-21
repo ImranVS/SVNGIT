2b969b016b8a1da359b1b9b3b74bf433f4e75245
sowjanya <sowjanya@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-12-21 07:51:26 +0000

    unused code
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@1975 6b605366-3ce6-ac4f-954a-f758cfb76b34

--- VitalSignsWeb/VitalSigns.Web/scripts/configurator/components/applicationSettings/application-settings-businesshours.component.ts
+++ VitalSignsWeb/VitalSigns.Web/scripts/configurator/components/applicationSettings/application-settings-businesshours.component.ts
@@ -1,17 +1,7 @@
 ﻿import {Component, OnInit, ViewChild} from '@angular/core';
-import { CommonModule } from '@angular/common';
 import {HttpModule}    from '@angular/http';
 import {RESTService} from '../../../core/services';
 import {GridBase} from '../../../core/gridBase';
-
-import {AppNavigator} from '../../../navigation/app.navigator.component';
-import * as wjFlexGrid from 'wijmo/wijmo.angular2.grid';
-import * as wjInputTime from 'wijmo/wijmo.angular2.input';
-import * as wjFlexGridFilter from 'wijmo/wijmo.angular2.grid.filter';
-import * as wjFlexGridGroup from 'wijmo/wijmo.angular2.grid.grouppanel';
-import * as wjFlexInput from 'wijmo/wijmo.angular2.input';
-import * as wjCoreModule from 'wijmo/wijmo.angular2.core';
-
 import {AppComponentService} from '../../../core/services';
 
 
