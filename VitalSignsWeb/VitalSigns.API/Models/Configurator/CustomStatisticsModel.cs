288fc11904b62aef701cbe2011f15f7f13423124
durga <durga@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-12-28 10:33:49 +0000

    VSPLUS-3423 Extend Domino Custom Statistics to compare string values
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@2006 6b605366-3ce6-ac4f-954a-f758cfb76b34

--- VitalSignsWeb/VitalSigns.API/Models/Configurator/CustomStatisticsModel.cs
+++ VitalSignsWeb/VitalSigns.API/Models/Configurator/CustomStatisticsModel.cs
@@ -8,20 +8,30 @@ namespace VitalSigns.API.Models.Configurator
 {
     public class CustomStatisticsModel
     {
-        [JsonProperty ("id")]
+        [JsonProperty("id")]
         public string Id { get; set; }
 
         [JsonProperty("domino_servers")]
         public List<string> DominoServers { get; set; }
 
-        [JsonProperty ("stat_name")]
+        [JsonProperty("stat_name")]
         public String StatName { get; set; }
 
         [JsonProperty("yellow_threshold")]
         public String ThresholdValue { get; set; }
 
+        [JsonProperty("type_of_statistic")]
+        public String TypeOfStatistic { get; set; }
+
         [JsonProperty("greater_than_or_less_than")]
         public String GreaterThanOrLessThan { get; set; }
+
+        //[JsonProperty("numeric_or_string")]
+        //public String NumericOrString { get; set; }
+
+        [JsonProperty("eual_or_not_equal")]
+        public String EqualOrNotEqual { get; set; }
+
 
         [JsonProperty("times_in_a_row")]
         public int? TimesInARow { get; set; }
