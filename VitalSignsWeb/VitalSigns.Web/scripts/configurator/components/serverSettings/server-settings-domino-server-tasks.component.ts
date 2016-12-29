0b6687e882c6bdc329b64d0b0c29b21c05d84af1
sowjanya <sowjanya@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-12-29 12:25:18 +0000

    VSPLUS-3444 Configurator: SSE/Domino Server Tasks - add task does not work
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@2019 6b605366-3ce6-ac4f-954a-f758cfb76b34

--- VitalSignsWeb/VitalSigns.Web/scripts/configurator/components/serverSettings/server-settings-domino-server-tasks.component.ts
+++ VitalSignsWeb/VitalSigns.Web/scripts/configurator/components/serverSettings/server-settings-domino-server-tasks.component.ts
@@ -1,4 +1,4 @@
-﻿import {Component, OnInit} from '@angular/core';
+﻿import {Component, OnInit,ViewChild} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {HttpModule}    from '@angular/http';
 import {RESTService} from '../../../core/services';
@@ -24,24 +24,57 @@ import {ServersLocationService} from './serverattributes-view.service';
         RESTService, ServersLocationService
     ]
 })
-export class DominoServerTasks extends GridBase implements OnInit  {  
-    devices: string;
+export class DominoServerTasks implements OnInit  {  
+    protected service: RESTService;
+    protected appComponentService: AppComponentService;
+    devices: string = "";
     currentDeviceType:string="Domino"
-   
+    @ViewChild('flex') flex: wijmo.grid.FlexGrid;
+    data: wijmo.collections.CollectionView;
     currentForm: FormGroup;
-    constructor(service: RESTService, private formBuilder: FormBuilder, appComponentService: AppComponentService) {
-        super(service, appComponentService);
+    constructor(service: RESTService, private formBuilder: FormBuilder, appComponentService: AppComponentService) {  
+        this.service = service; 
+        this.appComponentService = appComponentService;     
         this.currentForm = this.formBuilder.group({
             'setting': [''],
             'value': [''],
             'devices': ['']
-
-
         });
-        this.formName = "DominoServerTasks";
-    }  
+    } 
+  get pageSize(): number {
+        return this.data.pageSize;
+    }
+    set pageSize(value: number) {
+        if (this.data.pageSize != value) {
+            this.data.pageSize = value;
+            if (this.flex) {
+                (<wijmo.collections.IPagedCollectionView>this.flex.collectionView).pageSize = value;
+            }
+        }
+  }
+
+    selectionChangedHandler = () => {
+        console.log(this.flex.collectionView.currentItem);
+        (<wijmo.collections.CollectionView>this.flex.collectionView).commitEdit()
+       
+    }
+
     ngOnInit() {
-        this.initialGridBind('/Configurator/get_domino_server_tasks');
+       
+ this.service.get('/Configurator/get_domino_server_tasks')
+            .subscribe(
+            response => {
+                if (response.status == "Success") {
+                    this.data = new wijmo.collections.CollectionView(new wijmo.collections.ObservableArray(response.data));
+                    this.data.pageSize = 10;
+                } else {
+                    this.appComponentService.showErrorMessage(response.message);
+                }
+
+            }, error => {
+                var errorMessage = <any>error;
+                this.appComponentService.showErrorMessage(errorMessage);
+            });
     } 
     changeInDevices(devices: string) {
         this.devices = devices;
@@ -61,7 +94,9 @@ export class DominoServerTasks extends GridBase implements OnInit  {
     buildPostData(setting: string) {
         var slectedDominoServerValues: DominoServerTasksValue[] = [];
         for (var _i = 0; _i < this.flex.collectionView.sourceCollection.length; _i++) {
+          // console.log(this.flex.collectionView.sourceCollection.length);
             var item = (<wijmo.collections.CollectionView>this.flex.collectionView.sourceCollection)[_i];
+            console.log(item);
             if (item.is_selected) {
                 var dominoserverObject = new DominoServerTasksValue();
                 dominoserverObject.id = item.id;
@@ -71,6 +106,7 @@ export class DominoServerTasks extends GridBase implements OnInit  {
                 dominoserverObject.is_disallow = item.is_disallow;
                 dominoserverObject.task_name = item.task_name;
                 slectedDominoServerValues.push(dominoserverObject);
+                console.log(dominoserverObject);
             }
 
         }
