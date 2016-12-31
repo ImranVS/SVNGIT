d258d9b4692335c33a4dd6d526cc669183fa7d00
nshkarayeva <nshkarayeva@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2016-12-31 02:35:45 +0000

    VSPLUS-3289
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@2040 6b605366-3ce6-ac4f-954a-f758cfb76b34

--- VitalSignsWeb/VitalSigns.Web/scripts/configurator/components/mobileusers/mobile-users.component.ts
+++ VitalSignsWeb/VitalSigns.Web/scripts/configurator/components/mobileusers/mobile-users.component.ts
@@ -1,22 +1,33 @@
-﻿import {Component, OnInit, ViewChild, AfterViewInit} from '@angular/core';
-import {RESTService} from '../../../core/services';
+﻿import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RESTService } from '../../../core/services';
 import {GridBase} from '../../../core/gridBase';
 import {AppComponentService} from '../../../core/services';
 
 @Component({
     templateUrl: '/app/configurator/components/mobileusers/mobile-users.component.html',
-    providers: [RESTService]
+    providers: [
+        HttpModule,
+        RESTService
+    ]
 })
 export class MobileUser extends GridBase implements OnInit {
     sererNames: any;
     errorMessage: any;
     currentEditItem: any;
     mobileDeviceData: wijmo.collections.CollectionView;
-    
+    formObject: any = {
+        id: null,
+        threshold: null
+    };
+
     constructor(service: RESTService, appComponentService: AppComponentService) {
   
         super(service, appComponentService);
-        this.formName = "Critical Devices";
+        this.formName = "Critical Device";
         
       
     }
@@ -34,8 +45,33 @@ export class MobileUser extends GridBase implements OnInit {
     saveMobileUsers(dlg: wijmo.input.Popup) {      
         this.saveGridRow('/configurator/save_mobileusers', dlg);
     }
+    saveMobileUsersMonitor(dlg: wijmo.input.Popup) {
+        var saveUrl = '/configurator/save_mobileusers';
+        if (this.formObject.id != "") {
+            this.service.put(saveUrl, this.formObject)
+                .subscribe(
+                response => {
+                    //assign new data sets to each grid based on the response data
+                },
+                (error) => this.errorMessage = <any>error
+                );
+            (<wijmo.collections.CollectionView>this.mobileDeviceGrid.collectionView).commitEdit();
+        }
+        dlg.hide();
+    }
     delteMobileUsers() {
         this.delteGridRow('/configurator/delete_mobile_users/');
+    }
+    showEditForm(dlg: wijmo.input.Popup) {
+
+        this.formTitle = "Edit " + this.formName;
+
+        this.formObject.id = this.mobileDeviceGrid.collectionView.currentItem.id;
+        this.formObject.treshold = 20;
+        
+        (<wijmo.collections.CollectionView>this.mobileDeviceGrid.collectionView).editItem(this.mobileDeviceGrid.collectionView.currentItem);
+        this.showDialog(dlg);
+
     }
     get MobileDevicepageSize(): number {
         return this.mobileDeviceData.pageSize;
