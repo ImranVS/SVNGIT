467b57569c09187fa574fd5703f5a0a3cf0aa1ef
swathid <swathid@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2017-01-05 06:42:54 +0000

    Vsplus-3464
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@2086 6b605366-3ce6-ac4f-954a-f758cfb76b34

--- VitalSignsWeb/VitalSigns.Web/scripts/configurator/components/applicationSettings/application-settings-locations.component.ts
+++ VitalSignsWeb/VitalSigns.Web/scripts/configurator/components/applicationSettings/application-settings-locations.component.ts
@@ -1,15 +1,8 @@
 ﻿import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, FormControl, Validators} from '@angular/forms';
-import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import {HttpModule}    from '@angular/http';
 import {RESTService} from '../../../core/services';
-import {AppNavigator} from '../../../navigation/app.navigator.component';
-import * as wjFlexGrid from 'wijmo/wijmo.angular2.grid';
-import * as wjFlexGridFilter from 'wijmo/wijmo.angular2.grid.filter';
-import * as wjFlexGridGroup from 'wijmo/wijmo.angular2.grid.grouppanel';
-import * as wjFlexInput from 'wijmo/wijmo.angular2.input';
-import * as wjCoreModule from 'wijmo/wijmo.angular2.core';
 import {GridBase} from '../../../core/gridBase';
 import {AppComponentService} from '../../../core/services';
 
@@ -86,7 +79,21 @@ export class Location extends GridBase implements OnInit  {
    
     deltelocations() {
 
-        this.delteGridRow('/Configurator/delete_location/');
+
+       // this.delteGridRow('/Configurator/delete_location/');
+        this.service.delete('/Configurator/delete_location/' + this.flex.collectionView.currentItem.id)
+            .subscribe(
+            response => {
+                if (response.status == "Success") {
+                    this.appComponentService.showSuccessMessage(response.message);
+                } else {
+                    this.appComponentService.showErrorMessage(response.message);
+                }
+
+            }, error => {
+                var errorMessage = <any>error;
+                this.appComponentService.showErrorMessage(errorMessage);
+            });
 
     }
 
