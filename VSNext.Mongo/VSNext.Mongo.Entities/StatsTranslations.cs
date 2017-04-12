74a6314bc2b9ecf1f67bc7434aef407f0da651cf
wstanulis <wstanulis@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2017-04-12 23:03:00 +0000

    Stats Translation Commit
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@2455 6b605366-3ce6-ac4f-954a-f758cfb76b34

---
+++ VSNext.Mongo/VSNext.Mongo.Entities/StatsTranslations.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VSNext.Mongo.Repository;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Runtime.Serialization;
+
+namespace VSNext.Mongo.Entities
+{
+    [DataContract]
+    [Serializable]
+    [CollectionName("stats_translation")]
+    public class StatsTranslation : Entity
+    {
+        [DataMember]
+        [BsonElement("stat_name")]
+        [BsonIgnoreIfNullAttribute]
+        public string StatName { get; set; }
+
+        [DataMember]
+        [BsonElement("translated_name")]
+        [BsonIgnoreIfNullAttribute]
+        public string TranslatedName { get; set; }
+        
+    }
+}
