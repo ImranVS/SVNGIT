cfcdbf08f3f46da8d01a2ec098fb001dee53a728
nshkarayeva <nshkarayeva@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2017-04-29 20:46:49 +0000

    VSPLUS-3568
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@2499 6b605366-3ce6-ac4f-954a-f758cfb76b34

--- VitalSignsWeb/VitalSigns.Web/scripts/configurator/components/serverImport/server-import-domino.component.ts
+++ VitalSignsWeb/VitalSigns.Web/scripts/configurator/components/serverImport/server-import-domino.component.ts
@@ -151,5 +151,22 @@ export class DominoServerImport implements OnInit{
              server.is_selected = false;
          }
      }
+
+     isTrue(value) {
+         if (typeof (value) == 'string') {
+             value = value.toLowerCase();
+         }
+         switch (value) {
+             case true:
+             case "true":
+             case 1:
+             case "1":
+             case "on":
+             case "yes":
+                 return true;
+             default:
+                 return false;
+         }
+     }
 }
     
