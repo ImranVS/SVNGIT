85fecd1e2124f7adfd130a51e8a64a5af192f51d
nshkarayeva <nshkarayeva@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2017-05-11 16:26:19 +0000

    Exchange page.
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@2554 6b605366-3ce6-ac4f-954a-f758cfb76b34

---
+++ VitalSignsWeb/VitalSigns.Web/scripts/configurator/components/server/server-exchange-services.component.ts
@@ -0,0 +1,71 @@
+﻿import {Component, OnInit, ViewChild,Input} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {HttpModule}    from '@angular/http';
+import {RESTService} from '../../../core/services';
+import {GridBase} from '../../../core/gridBase';
+
+import {AppNavigator} from '../../../navigation/app.navigator.component';
+import * as wjFlexGrid from 'wijmo/wijmo.angular2.grid';
+import * as wjFlexGridFilter from 'wijmo/wijmo.angular2.grid.filter';
+import * as wjFlexGridGroup from 'wijmo/wijmo.angular2.grid.grouppanel';
+import * as wjFlexInput from 'wijmo/wijmo.angular2.input';
+import * as wjCoreModule from 'wijmo/wijmo.angular2.core';
+import {ActivatedRoute} from '@angular/router';
+import {FormBuilder, FormGroup, FormControl, Validators} from '@angular/forms';
+import {AppComponentService} from '../../../core/services';
+
+
+@Component({
+    templateUrl: '/app/configurator/components/server/server-exchange-services.component.html',
+    providers: [
+        HttpModule,
+        RESTService
+    ]
+})
+export class Services extends GridBase implements OnInit {
+    selectedServers: string;
+    deviceId: any;
+    data: wijmo.collections.CollectionView;
+    servicesdata: any;
+    errorMessage: string;
+    currentForm: FormGroup;
+    selectedSettingValue: any;
+    selectedName: string;
+
+    constructor(service: RESTService, private formBuilder: FormBuilder, private route: ActivatedRoute, appComponentService: AppComponentService) {
+        super(service, appComponentService);
+    }
+
+    ngOnInit() {
+        this.route.params.subscribe(params => {
+            this.deviceId = params['service'];
+        });
+        //this.service.get('/Configurator/get_windows_services')
+        //    .subscribe(
+        //    (response) => {
+        //        this.servicesdata = response.data;
+        //    },
+        //    (error) => this.errorMessage = <any>error
+        //    );
+        this.initialGridBind('/Configurator/get_windows_services');
+    }
+
+    buildPostData(setting: string, dlg) {
+        var postData = {
+            "setting": setting,
+
+        };
+        this.currentForm.setValue(postData);
+
+        //this.saveGridRow('/configurator/save_server_tasks', postData, dlg)
+        this.service.put('/Configurator/save_domino_server_tasks', postData)
+            .subscribe(
+            response => {
+
+            });
+
+    }
+}
+
+
+
