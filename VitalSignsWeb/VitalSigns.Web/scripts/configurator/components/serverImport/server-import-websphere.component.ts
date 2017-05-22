065786b390d45e422c518ec40e144ebac8fbe403
nshkarayeva <nshkarayeva@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2017-05-22 23:11:07 +0000

    VSPLUS-3614
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@2580 6b605366-3ce6-ac4f-954a-f758cfb76b34

--- VitalSignsWeb/VitalSigns.Web/scripts/configurator/components/serverImport/server-import-websphere.component.ts
+++ VitalSignsWeb/VitalSigns.Web/scripts/configurator/components/serverImport/server-import-websphere.component.ts
@@ -43,6 +43,7 @@ export class WebSphereServerImport extends GridBase implements OnInit {
                 this.data = new wijmo.collections.CollectionView(new wijmo.collections.ObservableArray(response.data.cellData));
                 if (response.data.cellData.length > 0) {
                     this.webSphereServerNodeData = new wijmo.collections.CollectionView(new wijmo.collections.ObservableArray(response.data.cellData[0].nodes_data));
+                    this.webSphereServerNodeData.groupDescriptions.push(new wijmo.collections.PropertyGroupDescription("cell_name"));
                     this.webSphereServerNodeData.groupDescriptions.push(new wijmo.collections.PropertyGroupDescription("node_name"));               
                     this.webSphereServerNodeData.pageSize = 10;                   
                 }
@@ -82,7 +83,7 @@ export class WebSphereServerImport extends GridBase implements OnInit {
         this.service.put('/configurator/get_websohere_nodes', this.flex.collectionView.currentItem)
             .subscribe(
             response => {
-             
+                            
             },
             (error) => this.errorMessage = <any>error
 
