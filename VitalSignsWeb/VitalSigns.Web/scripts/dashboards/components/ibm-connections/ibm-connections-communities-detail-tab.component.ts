ab3e35b1f560bec6776d3f269df13ee1446fde30
aforbes <aforbes@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2017-06-23 21:47:08 +0000

    New tab for IBM Communities
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@2662 6b605366-3ce6-ac4f-954a-f758cfb76b34

---
+++ VitalSignsWeb/VitalSigns.Web/scripts/dashboards/components/ibm-connections/ibm-connections-communities-detail-tab.component.ts
@@ -0,0 +1,54 @@
+﻿import {Component, ComponentFactoryResolver, OnInit, Injector } from '@angular/core';
+
+import {WidgetController, WidgetContainer, WidgetContract} from '../../../core/widgets';
+import {WidgetService} from '../../../core/widgets/services/widget.service';
+
+import {ServiceTab} from '../../../services/models/service-tab.interface';
+
+import {IBMConnectionsGrid} from './ibm-connections-grid.component';
+import {ActivatedRoute} from '@angular/router';
+
+declare var injectSVG: any;
+
+@Component({
+    selector: 'tab-communities-detail',
+    templateUrl: '/app/dashboards/components/ibm-connections/ibm-connections-communities-detail-tab.component.html'
+})
+
+export class IBMConnectionsCommunitiesDetailTab extends WidgetController implements OnInit, ServiceTab {
+
+    widgets: WidgetContract[];
+    serviceId: string;
+    today: Date;
+
+    constructor(protected resolver: ComponentFactoryResolver, protected widgetService: WidgetService, private route: ActivatedRoute) {
+        super(resolver, widgetService);
+    }
+
+    
+    
+    ngOnInit() {
+        this.route.params.subscribe(params => {
+            if (params['service'])
+                this.serviceId = params['service'];
+            else {
+                this.serviceId = this.widgetService.getProperty('serviceId');
+            }
+        });
+        //var date = new Date();
+        //var displayDate = new Date(date.getFullYear(), date.getMonth(), date.getDate()).toISOString();
+
+        this.widgets = [
+         
+            {
+                id: 'communityDetailList',
+                title: 'Communities Detail List',
+                name: 'IBMConnectionsCommunitiesDetailGrid',
+                css: 'col-xs-12 col-sm-12 col-md-12 col-lg-12',
+            }
+        ];
+        injectSVG();
+    }
+
+
+}
