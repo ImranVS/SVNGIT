112afb2b3cf98bcf1f1abd13b0048cf7459f506f
wstanulis <wstanulis@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2017-07-13 18:22:45 +0000

    Fixed a few bugs with Mail Volume report and Notes Database Grid
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@2713 6b605366-3ce6-ac4f-954a-f758cfb76b34

--- VitalSignsWeb/VitalSigns.Web/scripts/reports/components/mail/mail-volume-report.component.ts
+++ VitalSignsWeb/VitalSigns.Web/scripts/reports/components/mail/mail-volume-report.component.ts
@@ -1,4 +1,4 @@
-﻿import { Component, ComponentFactoryResolver, OnInit, ViewChild} from '@angular/core';
+﻿import { Component, ComponentFactoryResolver, OnInit} from '@angular/core';
 import {WidgetController, WidgetContract} from '../../../core/widgets';
 import {WidgetService} from '../../../core/widgets/services/widget.service';
 import {RESTService} from '../../../core/services/rest.service';
@@ -18,7 +18,6 @@ declare var injectSVG: any;
     ]
 })
 export class MailVolumeReport extends WidgetController {
-    @ViewChild('flex') flex: wijmo.grid.FlexGrid;
     contextMenuSiteMap: any;
     widgets: WidgetContract[];
     gridUrl: string = `/reports/summarystats_aggregation?type=Domino&aggregationType=sum&statName=[Mail.Transferred,Mail.TotalRouted,Mail.Delivered]`;
@@ -32,10 +31,6 @@ export class MailVolumeReport extends WidgetController {
 
     }
 
-    ExportExcel(event) {
-        let flex = this.flex;
-        wijmo.grid.xlsx.FlexGridXlsxConverter.save(this.flex, { includeColumnHeaders: true, includeCellStyles: false }, "Issues.xlsx");
-    }
     ngOnInit() {
 
         this.service.get('/navigation/sitemaps/mail_reports')
