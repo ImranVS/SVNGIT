1b759ae0b6201b9ffef096b43239b43fc1dcbd36
wstanulis <wstanulis@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2017-09-08 16:27:31 +0000

    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@2816 6b605366-3ce6-ac4f-954a-f758cfb76b34

---
+++ VitalSignsWeb/VitalSigns.API/Models/Configurator/Office365Node.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace VitalSigns.API.Models
+{
+    public class Office365Node
+    {
+
+        [JsonProperty("node_id")]
+        public string NodeId { get; set; }
+
+        [JsonProperty("is_selected")]
+        public bool IsSelected { get; set; }
+
+        [JsonProperty("location")]
+        public string Location { get; set; }
+    
+        [JsonProperty("host_name")]
+        public string HostName { get; set; }
+        
+    }
+}
