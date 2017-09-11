1d226bf02a253c576104bdecee6555179885ec96
aforbes <aforbes@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2017-09-11 15:54:14 +0000

    added new property to Sametime server
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@2832 6b605366-3ce6-ac4f-954a-f758cfb76b34

--- MonitoredItems/AssemblyInfo.vb
+++ MonitoredItems/AssemblyInfo.vb
@@ -1,4 +1,4 @@
-﻿Imports System
+Imports System
 Imports System.Reflection
 Imports System.Runtime.InteropServices
 
@@ -29,4 +29,4 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 
-<Assembly: AssemblyVersion("3.5.0")> 
+<Assembly: AssemblyVersion("3.0.0")> 
