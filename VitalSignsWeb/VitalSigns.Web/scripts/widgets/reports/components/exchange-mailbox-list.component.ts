1ab33aa7fe102d6f71c247b00ca423fc8ac6f37d
aforbes <aforbes@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2017-10-04 17:21:42 +0000

    VSPLUS-3698 Exchange Report:  Users approaching quota
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@2885 6b605366-3ce6-ac4f-954a-f758cfb76b34

---
+++ VitalSignsWeb/VitalSigns.Web/scripts/widgets/reports/components/exchange-mailbox-list.component.ts
@@ -0,0 +1,34 @@
+﻿import {Component, Input, OnInit} from '@angular/core';
+import {HttpModule}    from '@angular/http';
+
+import {WidgetComponent} from '../../../core/widgets';
+import {RESTService} from '../../../core/services';
+
+import {MailFileStatistic} from '../models/mail-file-statistic';
+
+
+@Component({
+    templateUrl: './app/widgets/reports/components/exchange-mailbox-list.component.html',
+    providers: [
+        HttpModule,
+        RESTService
+    ]
+})
+export class ExchnagemailboxList implements WidgetComponent, OnInit {
+    @Input() settings: any;
+    errorMessage: string;
+    data: any;
+    constructor(private service: RESTService) { }
+
+    loadData() {
+        this.service.get(`/reports/exchnage_mailboxes_prohbited_warning`)
+            .subscribe(
+            data => this.data = data.data,
+            error => this.errorMessage = <any>error
+            );
+    }
+
+    ngOnInit() {
+        this.loadData();
+    }
+}
