ec5a48ac0010046e9ea176ef2019de6571bff19f
achuta <achuta@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2017-11-21 23:58:39 +0000

    VSPLUS-3703  Ability to send scheduled reports
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@3035 6b605366-3ce6-ac4f-954a-f758cfb76b34

---
+++ VSNext.Mongo/VSNext.Mongo.Entities/SiteMap.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.Serialization;
+using VSNext.Mongo.Repository;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Collections.Generic;
+
+namespace VSNext.Mongo.Entities
+{
+    [DataContract]
+        [Serializable]
+        [CollectionName("sitemap")]
+        public class SiteMap : Entity
+        {
+
+            public SiteMap()
+            {
+                System.Reflection.PropertyInfo[] props = this.GetType().GetProperties(System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.DeclaredOnly);
+                foreach (var property in props)
+                {
+                    property.SetValue(this, null);
+                }
+
+            }
+         [BsonElement("title")]
+        public string Title { get; set; }
+
+        [BsonElement("nodes")]
+        public ICollection<SiteMapNode> Nodes { get; set; }
+    }
+}
