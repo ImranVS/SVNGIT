7ddd5096060a5dc5b10e6860d2af584c9887a2e4
wstanulis <wstanulis@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2018-01-26 17:19:20 +0000

    Users Mailboxes
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@3174 6b605366-3ce6-ac4f-954a-f758cfb76b34

---
+++ PowerShell_Scripts/EX_GetMailboxPermissions.ps1
@@ -0,0 +1,6 @@
+﻿param(
+        [ValidateNotNullOrEmpty()]
+        [string]$MailboxName
+    )
+$mbs = Get-Mailbox $MailboxName | Get-MailboxPermission | Where { ($_.IsInherited -eq $False) -and -not ($_.User -like "NT AUTHORITY\SELF") } 
+foreach($mb in $mbs){ New-Object PSObject -Property @{ Identity=$mb.Identity; Name=(Get-User $mb.user).Id}}
