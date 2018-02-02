055f3f094b841b11c957acf89e2d1cf4558b5fb2
wstanulis <wstanulis@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2018-02-02 15:52:00 +0000

    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@3185 6b605366-3ce6-ac4f-954a-f758cfb76b34

---
+++ VitalSignsWeb/VitalSigns.API/wwwroot/PowerShellScripts/Exchange/GetAdvancedMailboxStatistics.ps1
@@ -0,0 +1,12 @@
+﻿<#
+.DESCRIPTION
+Moves a mailbox to another database
+
+.SUBTYPES
+[Mailbox]
+#>
+param(
+        [ValidateNotNullOrEmpty()]
+        [string]$SamAccountName
+    )
+	Get-MailboxStatistics -Identity $SamAccountName
