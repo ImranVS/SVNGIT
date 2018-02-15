f8e1aa977c830c92ebd5f71a3897010536545da8
aforbes <aforbes@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2018-02-15 22:13:51 +0000

    Added new server types to the list
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@3229 6b605366-3ce6-ac4f-954a-f758cfb76b34

--- VitalSignsWeb/VitalSigns.Web/scripts/configurator/components/applicationSettings/application-settings-maintenance.component.ts
+++ VitalSignsWeb/VitalSigns.Web/scripts/configurator/components/applicationSettings/application-settings-maintenance.component.ts
@@ -37,7 +37,7 @@ export class Maintenance extends GridBase implements OnInit  {
     devices: string[] = [];
     _deviceList: any;
     checkedDevices: any;
-    selDeviceTypes: string = "Domino,Sametime,URL,WebSphere,IBM Connections";
+    selDeviceTypes: string = "Domino,Sametime,URL,WebSphere,IBM Connections,Exchange,SharePoint,Active Directory,Office365,Database Availability Group";
     keyUsers: string[] = [];
     loading = false;
 
