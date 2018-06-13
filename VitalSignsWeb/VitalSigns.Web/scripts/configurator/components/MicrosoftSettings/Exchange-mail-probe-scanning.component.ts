bacfc280ac61e6886da2b11f953e292ddfbca7ed
aforbes <aforbes@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2018-06-13 19:06:11 +0000

    Exchange Mail Probe Configurator page changes
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@3368 6b605366-3ce6-ac4f-954a-f758cfb76b34

--- VitalSignsWeb/VitalSigns.Web/scripts/configurator/components/MicrosoftSettings/Exchange-mail-probe-scanning.component.ts
+++ VitalSignsWeb/VitalSigns.Web/scripts/configurator/components/MicrosoftSettings/Exchange-mail-probe-scanning.component.ts
@@ -1,4 +1,5 @@
-﻿import {Component, OnInit,ViewChild} from '@angular/core';
+﻿import { Component, OnInit, ViewChild} from '@angular/core';
+import { FormControl, NgModel, NgForm } from '@angular/forms';
 import { RESTService } from '../../../core/services';
 import { HttpModule } from '@angular/http';
 import {GridBase} from '../../../core/gridBase';
@@ -21,15 +22,27 @@ declare var injectSVG: any;
 
     ]
 })
-export class ExchnageMailProbe extends GridBase implements OnInit {
+export class ExchnageMailProbe extends GridBase implements OnInit{
     @ViewChild('flex') flex: wijmo.grid.FlexGrid;
     @ViewChild('flex1') flex1: wijmo.grid.FlexGrid;
+
+    @ViewChild('redThreshold')
+    redFormControl: NgModel;
+
+    @ViewChild('frmUser')
+    userForm: NgForm;
+
+    @ViewChild('yellowThreshold')
+    yellowFormControl: NgModel;
+
     exchangedata: wijmo.collections.CollectionView;
     EventsName: any;
     errorMessage: string;
     currentPageSize: any = 20;
     checkedDevices: any;
     devices: string = "";
+
+    
    
     constructor(service: RESTService, private route: ActivatedRoute,private router: Router, appComponentService: AppComponentService, protected gridHelpers: gridHelpers.CommonUtils, private authService: AuthenticationService) {
         super(service, appComponentService);
@@ -45,6 +58,26 @@ export class ExchnageMailProbe extends GridBase implements OnInit {
             (error) => this.errorMessage = <any>error
             );
 
+    }
+
+    handleShownEvent(event) {
+        this.redFormControl.control.valueChanges.subscribe((value) => {
+            this.currentEditItem.mailprobe_red_threshold = parseInt('' + this.currentEditItem.mailprobe_red_threshold);
+            if (parseInt(value) > this.currentEditItem.mailprobe_yellow_threshold) {
+                this.yellowFormControl.control.setErrors(null);
+            } else {
+                this.yellowFormControl.control.setErrors({ 'invalid': true });
+            }
+        });
+
+        this.yellowFormControl.control.valueChanges.subscribe((value) => {
+            this.currentEditItem.mailprobe_red_threshold = parseInt('' + this.currentEditItem.mailprobe_red_threshold);
+            if (parseInt(value) < this.currentEditItem.mailprobe_red_threshold) {
+                this.redFormControl.control.setErrors(null);
+            } else {
+                this.redFormControl.control.setErrors({ 'invalid': true });
+            }
+        });
     }
 
     get pageSize(): number {
@@ -81,6 +114,8 @@ export class ExchnageMailProbe extends GridBase implements OnInit {
         this.initialGridBind('/configurator/get_exchange_mail_probe');
         injectSVG();
     }
+    
+  
     initialGridBind(dataURI: string) {
         this.service.get(dataURI)
             .subscribe(
@@ -111,9 +146,10 @@ export class ExchnageMailProbe extends GridBase implements OnInit {
          this.exchangedata.items.forEach(function (x) {
          x.is_selected = false;
         });
+        
          this.exchangedata.refresh();
+         this.showDialog(dlg);
     }
-
     saveExchangeMailProbes(dlg: wijmo.input.Popup) {
 
         var selectedExchangeServers: any[] = this.flex1.collectionView.items.filter(function (x) {
@@ -123,47 +159,88 @@ export class ExchnageMailProbe extends GridBase implements OnInit {
             });
 
         if (selectedExchangeServers.length < 2) {
-
             this.errorMessage = "Please select at least two servers";
         }
         else {
-            
                 var postdata = {
                     exchange_mail_probe: this.currentEditItem,
                     exchange_servers: selectedExchangeServers
-                }
-               
-                this.saveGridRow('/configurator/save_exchange_probes', dlg, postdata);
-               
-            
-
-            
+                }  
+               this.saveGridRow('/configurator/save_exchange_probes', dlg, postdata);
+                        
         }
-        
+       
+    } 
+
+    saveGridRow(saveUrl: any, dlg?: wijmo.input.Popup, postdata?: any) {
+
+        if (this.currentEditItem.id == "") {
+
+            this.service.put(saveUrl, postdata ? postdata : this.currentEditItem)
+
+                .subscribe(
+                response => {
+                    this.data = new wijmo.collections.CollectionView(new wijmo.collections.ObservableArray(response.data.mailprobes));
+                    this.exchangedata = new wijmo.collections.CollectionView(new wijmo.collections.ObservableArray(response.data.exchangeservers));
+                    if (response.status == "Success") {
+                        this.currentEditItem.id = response.data;
+                        (<wijmo.collections.CollectionView>this.flex.collectionView).commitNew();
+
+                        dlg.hide();
+                        this.initialGridBind('/configurator/get_exchange_mail_probe');
+                        this.appComponentService.showSuccessMessage(response.message);
+
+                    } else {
+
+                        this.appComponentService.showErrorMessage(response.message);
+                    }
+
+                }, error => {
+                    var errorMessage = <any>error;
+                    this.appComponentService.showErrorMessage(errorMessage);
+                });
+        }
+        else {
+            this.flex.collectionView.currentItem = this.currentEditItem;
+
+            this.service.put(saveUrl, postdata ? postdata : this.currentEditItem)
+                .subscribe(
+                response => {
+
+                    if (response.status == "Success") {
+                        (<wijmo.collections.CollectionView>this.flex.collectionView).commitEdit()
+
+                        dlg.hide();
+                        this.appComponentService.showSuccessMessage(response.message);
+                    } else {
+                        this.appComponentService.showErrorMessage(response.message);
+                    }
+
+                }, error => {
+                    var errorMessage = <any>error;
+                    this.appComponentService.showErrorMessage(errorMessage);
+                });
+
+        }
+
     }
-   
+
     deleteExchangeMailProbes() {
         this.deleteGridRow('/configurator/delete_exchange_mail_probe/');
     }
     onItemsSourceChanged() {
         var row = this.flex.columnHeaders.rows[0];
         row.wordWrap = true;
-        // autosize first header row
         this.flex.autoSizeRow(0, true);
 
     }
-
     editGridRow(dlg: wijmo.input.Popup) {
-        try {
+
+        console.log("Inside Edit Popup");
+        console.log(this.redFormControl);
+
             this.formTitle = "Edit " + this.formName;
             var mainThis = this;
-
-            //this.flex1.collectionView.items.forEach(function (x) {
-            //    x.is_selected = mainThis.flex.collectionView.currentItem.selected_exchange_servers.indexOf(x.id) > -1
-            //});
-
-            //flex1 = exchange servers
-            //flex = exchange mail probe servers
             
                 for(var i = 0; i < this.exchangedata.items.length; i++) {
                 var exchangeServer = this.exchangedata.items[i];
@@ -178,17 +255,7 @@ export class ExchnageMailProbe extends GridBase implements OnInit {
             (<wijmo.collections.CollectionView>this.flex.collectionView).editItem(this.flex.collectionView.currentItem);
             this.currentEditItem = this.flex.collectionView.currentItem;
             this.showDialog(dlg);
-        }
-        catch (ex) {
-            console.log(ex)
-        }
-        }
-   
-
-
-    //changeInDevices(devices: any) {
-    //    this.devices = devices;
-    //    this.checkedDevices = devices;
-    //}
+       
+    }
 }
 
