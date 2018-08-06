f2be374384b6cf39680b94930f32ce69c5a86b44
wstanulis <wstanulis@6b605366-3ce6-ac4f-954a-f758cfb76b34>
2018-08-06 14:53:21 +0000

    Cleaned up AD and EX connect PS scripts
    git-svn-id: https://svn.jnitinc.com/svn/vsdev/branches/VitalSignsMongo@3440 6b605366-3ce6-ac4f-954a-f758cfb76b34

--- PowerShell_Scripts/AD_Connect.ps1
+++ PowerShell_Scripts/AD_Connect.ps1
@@ -1,7 +1,7 @@
-﻿$uname= 'jnittech\administrator';
-$Pwd='Pa$$w0rd';
+﻿$uname= 'jnittech\vsinstall';
+$Pwd='Admin123!';
 $secpasswd = ConvertTo-SecureString $Pwd -AsPlainText –Force;
 $cred=New-object -typename System.Management.Automation.PSCredential -argumentlist $uname,$secpasswd;
-$s = new-pssession -computer "jnittech-ad.jnittech.com" -Credential $cred
+$s = new-pssession -computer "USSVCADDC02.jnittech.com" -Credential $cred
 Invoke-Command -session $s -script { Import-Module ActiveDirectory }
 Import-PSSession -session $s -module ActiveDirectory 
